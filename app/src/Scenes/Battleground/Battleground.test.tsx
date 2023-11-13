d50dfc5e6c18dc0bccc64277901215eff5d88d8b
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-11-12 21:48:50 -0300

    shared events

--- app/src/Scenes/Battleground/Battleground.test.tsx
+++ app/src/Scenes/Battleground/Battleground.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './Battleground';
+import events from 'events';
 
 test('renders learn react link', () => {
-  render(<App />);
+
+  const emitter = new events.EventEmitter();
+  render(<App events={emitter} />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
