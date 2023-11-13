df53d4c542e6de20a450de5ad0cc6e9ae08fa53a
Leonardo Farroco <mana-battle@users.noreply.github.com>
2023-11-13 09:16:23 -0300

    game, ui layers

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -55,11 +55,29 @@ const game = new Phaser.Game({
   }
 });
 
+//window resize event
+window.addEventListener('resize', () => {
 
+  game.scale.resize(window.innerWidth, window.innerHeight);
 
-function preload() { }
+});
+
+function preload(this: Phaser.Scene) {
+
+  this.load.image('tilesets/pipoya', 'assets/tilesets/pipoya.png');
+  this.load.tilemapTiledJSON('maps/map1', 'assets/maps/map1/mapdata.json');
+
+}
 function create(this: Phaser.Scene) {
   console.log("hello there")
+
+  const map = this.make.tilemap({ key: 'maps/map1' });
+
+  const tiles = map.addTilesetImage('tilesets/pipoya', 'tilesets/pipoya');
+
+  if (!tiles) return;
+
+  const layer = map.createLayer(0, tiles, 0, 0);
 
   var graphics = this.add.graphics();
 
