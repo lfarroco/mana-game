1636940a91ff7adb11bd2b68ace29c79db9cea6c
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-11-19 20:17:26 -0300

    adding squad buttons

--- app/src/UI/Battleground/SelectedEntity/SelectedSquad.tsx
+++ app/src/UI/Battleground/SelectedEntity/SelectedSquad.tsx
@@ -1,3 +1,4 @@
+import { Button } from "react-bootstrap"
 import { Squad } from "../../../Models/Squad"
 import "./styles.css"
 const SelectedSquad = ({ squad }: { squad: Squad }) => {
@@ -8,11 +9,20 @@ const SelectedSquad = ({ squad }: { squad: Squad }) => {
 				className="img-fluid"
 				src={"assets/jobs/archer/portrait.png"} alt={squad.name} />
 		</div>
-		<div className="col col-8">
+		<div className="col col-6">
 
 			<h3>{squad.name}</h3>
 			<p>{squad.members.toString()}</p>
 		</div>
+		<div className="col col-4">
+			<Button className="col-12">
+				Move
+			</Button>
+			<Button className="col-12">
+				Details
+			</Button>
+		</div>
+
 	</div>
 
 }
