2e7f36b32207915be4816ca29eef27c8c97ba431
Leonardo Farroco <mana-battle@users.noreply.github.com>
2023-11-26 10:12:27 -0300

    chara textures

--- app/src/Scenes/Battleground/Map/createMapSquads.ts
+++ app/src/Scenes/Battleground/Map/createMapSquads.ts
@@ -2,16 +2,13 @@ import Phaser from "phaser";
 import { Chara, chara } from "../chara";
 import { getState } from "../BGState";
 
-export function createMapSquads(
-	scene: Phaser.Scene,
-	map: Phaser.Tilemaps.Tilemap
-): Chara[] {
+export function createMapSquads( scene: Phaser.Scene,): Chara[] {
 
 	const state = getState()
 
 	return state.squads
 		.filter(squad => squad.dispatched)
 		.map(squad =>
-			chara(squad.position.x, squad.position.y, scene, squad)
+			chara(scene, squad)
 		);
 }
