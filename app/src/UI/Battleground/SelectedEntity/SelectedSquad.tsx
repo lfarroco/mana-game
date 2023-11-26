29616e032cdc4928ab60d160aec845fc6625969f
Leonardo Farroco <mana-battle@users.noreply.github.com>
2023-11-25 23:05:24 -0300

    members in selected squad

--- app/src/UI/Battleground/SelectedEntity/SelectedSquad.tsx
+++ app/src/UI/Battleground/SelectedEntity/SelectedSquad.tsx
@@ -1,8 +1,9 @@
 import { Button } from "react-bootstrap"
-import { Squad } from "../../../Models/Squad"
+import { Squad, getMembers } from "../../../Models/Squad"
 import "./styles.css"
 import * as Signals from "../../../Models/Signals"
 import { FORCE_ID_PLAYER } from "../../../Models/Force"
+import { getState } from "../../../Scenes/Battleground/BGState"
 
 const SelectedSquad = ({
 	squad,
@@ -14,18 +15,24 @@ const SelectedSquad = ({
 
 	const isPlayerControlled = squad.force === FORCE_ID_PLAYER
 
-	return <div className="row" id="selected-entity">
-		<div className="col col-2">
-			<img
-				className="img-fluid"
-				src={"assets/jobs/archer/portrait.png"} alt={squad.name} />
-		</div>
-		<div className="col col-6">
+	const members = getMembers(squad)
 
-			<h3>{squad.name}</h3>
-			<p>{squad.members.toString()}</p>
+	return <div className="row" id="selected-entity">
+		<div className="col col-10"
+		>
+			{
+				members.map(unit =>
+					<img
+						key={`squad-member-${unit.id}`}
+						style={{ maxHeight: '100px' }}
+						className="img-fluid"
+						src={`assets/jobs/${unit.job}/portrait.png`}
+						alt={unit.name}
+					/>
+				)
+			}
 		</div>
-		<div className="col col-4">
+		<div className="col col-2">
 			{!isSelectingMoveTarget && <>
 				{isPlayerControlled && <Button
 					onClick={() => {
