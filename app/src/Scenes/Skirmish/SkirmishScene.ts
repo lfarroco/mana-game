4c82baabda963d74e21652bef4b4f7c1ec0ea9f3
Leonardo Farroco <mana-battle@users.noreply.github.com>
2023-12-01 11:32:34 -0300

    render squad based on members

--- app/src/Scenes/Skirmish/SkirmishScene.ts
+++ app/src/Scenes/Skirmish/SkirmishScene.ts
@@ -4,67 +4,17 @@ import { preload } from "./preload";
 import { Unit, makeUnit } from "../../Models/Unit";
 import { SpineGameObject } from "@esotericsoftware/spine-phaser";
 import { getState } from "../../Models/State";
-
-const sqdA = [
-	{
-		pos: { x: 1, y: 0 },
-		unit: makeUnit()
-	},
-	{
-		pos: { x: 1, y: 1 },
-		unit: makeUnit()
-	},
-	{
-		pos: { x: 1, y: 2 },
-		unit: makeUnit()
-	},
-	{
-		pos: { x: 0, y: 0 },
-		unit: makeUnit()
-	},
-	{
-		pos: { x: 0, y: 1 },
-		unit: makeUnit()
-	},
-]
-const sqdB = [
-	{
-		pos: { x: 1, y: 0 },
-		unit: { ...makeUnit(), force: "CPU" }
-	},
-	{
-		pos: { x: 1, y: 1 },
-		unit: { ...makeUnit(), force: "CPU" }
-	},
-	{
-		pos: { x: 1, y: 2 },
-		unit: { ...makeUnit(), force: "CPU" }
-	},
-	{
-		pos: { x: 0, y: 0 },
-		unit: { ...makeUnit(), force: "CPU" }
-	},
-	{
-		pos: { x: 0, y: 1 },
-		unit: { ...makeUnit(), force: "CPU" }
-	},
-]
+import { Squad, getMembers } from "../../Models/Squad";
 
 class SkirmishScene extends Phaser.Scene {
-	bg: Phaser.GameObjects.Image | null = null;
-	charas: SpineGameObject[] = [];
-	units: Unit[] = [];
-	squadA: string = "";
-	squadB: string = "";
 	constructor() {
 		super("SkirmishScene");
 
-		console.log("SkirmishScene constructor")
 		listeners([
 			[events.SKIRMISH_STARTED, (squadA: string, squadB: string) => {
-				this.squadA = squadA
-				this.squadB = squadB
-				this.scene.start()
+				this.scene.start(this, { squadA, squadB })
+
+
 			}
 			],
 			[events.SKIRMISH_ENDED, () => {
@@ -76,53 +26,64 @@ class SkirmishScene extends Phaser.Scene {
 	}
 
 	preload = preload;
-	create = () => {
+	create = ({ squadA, squadB }: { squadA: string, squadB: string }) => {
 
-		this.bg = this.add.image(0, 0, "bgs/castle").setOrigin(0, 0);
+		this.renderBackground();
 
-		const a = this.renderSquad(sqdA, true)
-		const b = this.renderSquad(sqdB, false)
-
-		this.children.sort("y")
-
-		this.units = [...sqdA, ...sqdB].map(spec => spec.unit)
-		this.charas = [...a, ...b]
-
-		this.combat();
+		this.combat(squadA, squadB)
 
 		//@ts-ignore
 		window.skirmish = this
 	}
 
 
+	private renderBackground() {
+		const bg = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'bgs/castle');
+		let scaleX = this.cameras.main.width / bg.width;
+		let scaleY = this.cameras.main.height / bg.height;
+		let scale = Math.max(scaleX, scaleY);
+		bg.setScale(scale).setScrollFactor(0);
+	}
+
 	update() {
 	}
 
 
-	renderSquad(units: { pos: { x: number, y: number }, unit: Unit }[], isLeft: boolean) {
-		return units.map(spec => {
+	renderSquad(squad: Squad, isLeft: boolean) {
+		const units = getMembers(squad)
+		const charas = units.map((unit, index) => {
 
-			const baseX = isLeft ? 200 : 850;
+			const baseX = isLeft ? 350 : 650;
 			const baseY = 450
-			const spacingX = 150
-			const spacingY = 100
+			const spacingY = 50
 			const diagonalOffset = isLeft ? 50 : -50
-			const backOffset = 0
 
 			const spine = createSpineBody(this,
-				baseX + (spec.pos.x * spacingX) - (spec.pos.y * diagonalOffset) - (!isLeft && spec.pos.x === 1 ? 300 : 0),
-				baseY + (spec.pos.y * spacingY) + (spec.pos.x === 0 ? backOffset : 0),
-				spec.unit).setName(spec.unit.id)
-			const scale = 0.3
+				baseX - (index * diagonalOffset),
+				baseY + (index * spacingY),
+				unit,
+			).setName(unit.id)
+			const scale = 0.25
 			spine.setScale(isLeft ? -scale : scale, scale)
 
 			return spine
 		})
+
+		return { units, charas }
 	}
 
-	combat() {
+	combat(squadA: string, squadB: string) {
+		const state = getState()
+		const sa = state.squads.find(squad => squad.id === squadA) as Squad
+		const sb = state.squads.find(squad => squad.id === squadB) as Squad
 
-		const initiative = this.units.map(unit => {
+		const a = this.renderSquad(sa, true);
+		const b = this.renderSquad(sb, false);
+
+		const charas = [...a.charas, ...b.charas]
+		const units = [...a.units, ...b.units]
+
+		const initiative = units.map(unit => {
 			return {
 				unit,
 				initiative: Math.random()
@@ -134,6 +95,9 @@ class SkirmishScene extends Phaser.Scene {
 		const combat = {
 			turn,
 			initiative,
+			squads: [sa.id, sb.id],
+			units,
+			charas,
 			actions: []
 		}
 
@@ -141,20 +105,24 @@ class SkirmishScene extends Phaser.Scene {
 
 	}
 
-	turn(combat: { turn: number, initiative: { unit: Unit, initiative: number }[], actions: any[] }) {
+	turn(combat: {
+		charas: SpineGameObject[],
+		squads: string[],
+		units: Unit[], turn: number, initiative: { unit: Unit, initiative: number }[], actions: any[]
+	}) {
 
 		const state = getState()
 
 		const currentUnit = combat.initiative[combat.turn].unit
 
-		const target = this.units.filter(unit => unit.force !== currentUnit.force)[0]
+		const target = combat.units.filter(unit => unit.force !== currentUnit.force)[0]
 
 		console.log(currentUnit, target)
 
 		const isLeft = currentUnit.force === "PLAYER"
 
-		const activeUnitSprite = this.charas.find(chara => chara.name === currentUnit.id)
-		const targetUnitSprite = this.charas.find(chara => chara.name === target.id)
+		const activeUnitSprite = combat.charas.find(chara => chara.name === currentUnit.id)
+		const targetUnitSprite = combat.charas.find(chara => chara.name === target.id)
 
 		if (!activeUnitSprite || !targetUnitSprite) return
 
@@ -181,7 +149,7 @@ class SkirmishScene extends Phaser.Scene {
 						if (combat.turn < 2) {
 							this.turn(combat)
 						} else {
-							emit(events.SKIRMISH_ENDED, this.squadA, this.squadB)
+							emit(events.SKIRMISH_ENDED, combat.squads[0], combat.squads[1])
 						}
 					}
 				});
