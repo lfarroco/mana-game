5b3ab681f50555fa38a43c60547b3fafb6c948f1
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-12-29 10:45:32 -0300

    Update squad positions and statuses

--- app/src/Models/State.ts
+++ app/src/Models/State.ts
@@ -1,7 +1,8 @@
 import { Engagement } from "../Systems/Engagement/Engagement";
 import { City } from "./City";
 import { FORCE_ID_PLAYER, Force, makeForce } from "./Force";
-import { Squad, makeSquad } from "./Squad";
+import { boardVec } from "./Misc";
+import { SQUAD_STATUS, Squad, makeSquad } from "./Squad";
 import { Unit, makeUnit } from "./Unit";
 
 export const initialState = (): State => ({
@@ -24,12 +25,40 @@ export const initialState = (): State => ({
 			name: "squad-1",
 			leader: "u1",
 			members: ["u1", "u2"],
+			status: SQUAD_STATUS.IDLE,
+			position: boardVec(1, 1)
 		},
 		{
 			...makeSquad("s2", FORCE_ID_PLAYER),
 			name: "squad-2",
 			leader: "u3",
-			members: ["u3", "u4"]
+			members: ["u3", "u4"],
+			status: SQUAD_STATUS.IDLE,
+			position: boardVec(1, 1)
+		},
+		{
+			...makeSquad("s3", FORCE_ID_PLAYER),
+			name: "squad-3",
+			leader: "u30",
+			members: ["u30", "u31"],
+			status: SQUAD_STATUS.IDLE,
+			position: boardVec(2, 0)
+		},
+		{
+			...makeSquad("s4", FORCE_ID_PLAYER),
+			name: "squad-4",
+			leader: "u40",
+			members: ["u40", "u41"],
+			status: SQUAD_STATUS.IDLE,
+			position: boardVec(3, 1)
+		},
+		{
+			...makeSquad("s5", FORCE_ID_PLAYER),
+			name: "squad-5",
+			leader: "u50",
+			members: ["u50", "u51"],
+			status: SQUAD_STATUS.IDLE,
+			position: boardVec(2, 2)
 		},
 	],
 	units: [
@@ -37,6 +66,12 @@ export const initialState = (): State => ({
 		{ ...makeUnit(), id: "u2", squad: "s1", force: FORCE_ID_PLAYER, job: "knight" },
 		{ ...makeUnit(), id: "u3", squad: "s2", force: FORCE_ID_PLAYER, job: "knight" },
 		{ ...makeUnit(), id: "u4", squad: "s2", force: FORCE_ID_PLAYER, job: "knight" },
+		{ ...makeUnit(), id: "u30", squad: "s3", force: FORCE_ID_PLAYER, job: "knight" },
+		{ ...makeUnit(), id: "u31", squad: "s3", force: FORCE_ID_PLAYER, job: "knight" },
+		{ ...makeUnit(), id: "u40", squad: "s4", force: FORCE_ID_PLAYER, job: "knight" },
+		{ ...makeUnit(), id: "u41", squad: "s4", force: FORCE_ID_PLAYER, job: "knight" },
+		{ ...makeUnit(), id: "u50", squad: "s5", force: FORCE_ID_PLAYER, job: "knight" },
+		{ ...makeUnit(), id: "u51", squad: "s5", force: FORCE_ID_PLAYER, job: "knight" },
 	],
 	cities: [],
 	selectedEntity: null,
