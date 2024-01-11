49d610843df74de3fcf83487ebd0194ca6bfcbe2
Leonardo Farroco <leonardo.farroco@gmail.com>
2024-01-11 16:52:56 -0300

    multi unit selection logic

--- app/src/Scenes/Battleground/Map/makeMapInteractive.ts
+++ app/src/Scenes/Battleground/Map/makeMapInteractive.ts
@@ -1,7 +1,8 @@
 import Phaser from "phaser";
 import BattlegroundScene from "../BattlegroundScene";
 import { emit, events } from "../../../Models/Signals";
-import { asBoardVec } from "../../../Models/Misc";
+import { BoardVec, asBoardVec, boardVec } from "../../../Models/Misc";
+import { FORCE_ID_PLAYER } from "../../../Models/Force";
 
 export function makeMapInteractive(
 	scene: BattlegroundScene,
@@ -27,6 +28,9 @@ export function makeMapInteractive(
 
 		if (pointer.upElement?.tagName !== "CANVAS") return;
 
+		// is middle mouse button
+		if (pointer.buttons !== 4) return;
+
 		if (pointer.downTime < 100) return;
 
 		if (pointer.distance < 10) return;
@@ -35,9 +39,80 @@ export function makeMapInteractive(
 		scene.cameras.main.scrollY = scene.cameras.main.scrollY - dragY;
 	});
 
+	let rectVecStart: BoardVec | null = null;
+	let selectionRect = scene.add.rectangle(0, 0, 0, 0, 0x00ff00, 0.5).setOrigin(0, 0)
+
+	bgLayer.on(Phaser.Input.Events.DRAG_START, (pointer: Phaser.Input.Pointer, dragX: number, dragY: number) => {
+
+		if (pointer.upElement?.tagName !== "CANVAS") return;
+
+		// is left mouse button
+		if (pointer.buttons !== 1) return;
+
+		rectVecStart = boardVec(dragX, dragY);
+
+		console.log("drag start", rectVecStart)
+
+
+	});
+
+	bgLayer.on(Phaser.Input.Events.DRAG, (pointer: Phaser.Input.Pointer, dragX: number, dragY: number) => {
+
+		if (pointer.upElement?.tagName !== "CANVAS") return;
+
+		// is left mouse button
+		if (pointer.buttons !== 1) return;
+
+		if (!rectVecStart) return;
+
+		if (!selectionRect) return;
+
+		const rectWidth = pointer.x - rectVecStart.x
+
+		const rectHeight = pointer.y - rectVecStart.y
+
+		selectionRect.setPosition(
+			rectVecStart.x,
+			rectVecStart.y
+		);
+
+		selectionRect.setSize(
+			rectWidth,
+			rectHeight
+		);
+
+		scene.charas.forEach(chara => {
+			// is inside selection rect
+			if (selectionRect?.getBounds().contains(chara.sprite.x, chara.sprite.y)) {
+				chara.sprite.setTint(0x00ff00)
+			}
+			else chara.sprite.setTint(0xffffff);
+		});
+
+	});
+
 	bgLayer.on(Phaser.Input.Events.DRAG_END, (pointer: Phaser.Input.Pointer, dragX: number, dragY: number) => {
 
 		if (pointer.upElement?.tagName !== "CANVAS") return;
+
+
+		// charas inside selection
+		const charas = scene.charas.filter(c => c.force === FORCE_ID_PLAYER).filter(chara => {
+			return selectionRect?.getBounds().contains(chara.sprite.x, chara.sprite.y);
+		});
+
+		charas.forEach(c => c.sprite.setTint(0xffffff))
+
+		charas.forEach(chara => {
+			chara.sprite.setTint(0xff0000);
+		});
+
+		emit(events.MULTIPLE_SQUADS_SELECTED, charas.map(c => c.id))
+
+		rectVecStart = null
+
+		selectionRect.setPosition(0, 0).setSize(0, 0);
+
 
 	});
 
