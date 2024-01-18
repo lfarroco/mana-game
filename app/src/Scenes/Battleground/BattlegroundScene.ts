691ec3240c8fc3f49bbbca74c2a922b4314692e2
Leonardo Farroco <leonardo.farroco@gmail.com>
2024-01-18 20:16:27 -0300

    formatting

--- app/src/Scenes/Battleground/BattlegroundScene.ts
+++ app/src/Scenes/Battleground/BattlegroundScene.ts
@@ -33,7 +33,7 @@ import { TURN_DURATION } from "../../config";
 
 export class BattlegroundScene extends Phaser.Scene {
   graphics: Phaser.GameObjects.Graphics | null = null;
-  charas: Chara[] = []
+  charas: Chara[] = [];
   layers: {
     background: Phaser.Tilemaps.TilemapLayer;
     obstacles: Phaser.Tilemaps.TilemapLayer;
@@ -42,7 +42,7 @@ export class BattlegroundScene extends Phaser.Scene {
   isPaused = false;
   isSelectingSquadMove = false; // TODO: we can move this into the state
   state: State;
-  cities: { city: City, sprite: Phaser.GameObjects.Image }[] = []
+  cities: { city: City; sprite: Phaser.GameObjects.Image }[] = [];
   tilemap: Phaser.Tilemaps.Tilemap | null = null;
 
   constructor() {
@@ -53,99 +53,111 @@ export class BattlegroundScene extends Phaser.Scene {
       [events.RESUME_GAME, this.resumeGame],
       //[events.UNITS_SELECTED, this.selectSquads],
       //[events.CITY_SELECTED, this.selectCity],
-      [events.SELECT_SQUAD_MOVE_START, () => { this.isSelectingSquadMove = true }],
-      [events.SELECT_SQUAD_MOVE_DONE, this.moveUnitsTo],
-      [events.SELECT_SQUAD_MOVE_CANCEL, () => { this.isSelectingSquadMove = false }],
-      [events.BATTLEGROUND_TICK, (tick: number) => {
-        if (!this.isPaused) {
-          processTick(this)
-        }
-      },
+      [
+        events.SELECT_SQUAD_MOVE_START,
+        () => {
+          this.isSelectingSquadMove = true;
+        },
       ],
-      [events.UPDATE_SQUAD, (squadId: string, sqd: Partial<Squad>) => {
+      [events.SELECT_SQUAD_MOVE_DONE, this.moveUnitsTo],
+      [
+        events.SELECT_SQUAD_MOVE_CANCEL,
+        () => {
+          this.isSelectingSquadMove = false;
+        },
+      ],
+      [
+        events.BATTLEGROUND_TICK,
+        (tick: number) => {
+          if (!this.isPaused) {
+            processTick(this);
+          }
+        },
+      ],
+      [
+        events.UPDATE_SQUAD,
+        (squadId: string, sqd: Partial<Squad>) => {
+          if (sqd.stamina === 0) {
+            emit(events.SQUAD_DESTROYED, squadId);
+          }
 
-        if (sqd.stamina === 0) {
-          emit(events.SQUAD_DESTROYED, squadId)
-        }
-
-        updateSquad(this.state)(squadId)(sqd)
-
-      }
-      ]
+          updateSquad(this.state)(squadId)(sqd);
+        },
+      ],
     ]);
 
-    this.state = getState()
+    this.state = getState();
 
-    StaminaRegen.init(this.state)
-    squadDestroyed(this)
-    VictorySystem.init(this)
-    AISystem.init()
-    EmoteSystem.init(this)
-
-    //@ts-ignore
-    window.state = this.state
+    StaminaRegen.init(this.state);
+    squadDestroyed(this);
+    VictorySystem.init(this);
+    AISystem.init();
+    EmoteSystem.init(this);
 
     //@ts-ignore
-    window.bg = this
+    window.state = this.state;
+
+    //@ts-ignore
+    window.bg = this;
   }
 
   preload = preload;
   create = () => {
-    console.log("BattlegroundScene create")
+    console.log("BattlegroundScene create");
     const { map, layers } = createMap(this);
 
     importMapObjects(this.state, map);
 
-    CharaDispatch.init(this)
-    CharaMovement.init(this.state)
+    CharaDispatch.init(this);
+    CharaMovement.init(this.state);
 
-    this.layers = layers
+    this.layers = layers;
     this.tilemap = map;
-    this.cities = createCities(this, this.state.cities)
-    this.createMapSquads()
+    this.cities = createCities(this, this.state.cities);
+    this.createMapSquads();
 
-    ControlsSystem.init(this)
-    makeMapInteractive(this, map, layers.background)
+    ControlsSystem.init(this);
+    makeMapInteractive(this, map, layers.background);
     FogOfWarSystem.init(this);
     CityCaptureSystem.init(this);
     CursorSystem.init(this);
-    CharaFaceDirection.init(this)
-    makeSquadsInteractive(this, this.charas)
-    makeCitiesInteractive(this, this.cities.map(c => c.sprite))
+    CharaFaceDirection.init(this);
+    makeSquadsInteractive(this, this.charas);
+    makeCitiesInteractive(
+      this,
+      this.cities.map((c) => c.sprite)
+    );
     MovemenArrows.init(this);
-    EntitySelection.init(this)
+    EntitySelection.init(this);
 
-    const grid = layers.obstacles.layer.data.map(row => row.map(tile => tile.index === -1 ? 0 : 1))
-    Pathfinding.init(grid)
+    const grid = layers.obstacles.layer.data.map((row) =>
+      row.map((tile) => (tile.index === -1 ? 0 : 1))
+    );
+    Pathfinding.init(grid);
 
     this.time.addEvent({
       delay: TURN_DURATION / this.state.speed,
       callback: () => {
-
         if (this.state.winner && !this.scene.isPaused()) {
-          this.scene.pause()
-          this.time.removeAllEvents()
-          return
+          this.scene.pause();
+          this.time.removeAllEvents();
+          return;
         }
 
         if (!this.isPaused) {
           this.state.tick++;
-          emit(events.BATTLEGROUND_TICK, this.state.tick)
+          emit(events.BATTLEGROUND_TICK, this.state.tick);
         }
-
       },
-      loop: true
+      loop: true,
     });
 
     //@ts-ignore
-    window.scene = this
-  }
+    window.scene = this;
+  };
 
   createMapSquads() {
-    this.state.squads
-      .forEach(squad =>
-        emit(events.DISPATCH_SQUAD, squad.id)
-      );
+    this.state.squads.forEach((squad) => emit(events.DISPATCH_SQUAD, squad.id));
   }
 
   // selectCity = (id: string) => {
@@ -155,25 +167,19 @@ export class BattlegroundScene extends Phaser.Scene {
 
   pauseGame = () => {
     this.isPaused = true;
-  }
+  };
   resumeGame = () => {
     this.isPaused = false;
-  }
+  };
   moveUnitsTo = (sqdIds: string[], { x, y }: Vec2) => {
-
-    const units = this.state.squads.filter(sqd => sqdIds.includes(sqd.id))
+    const units = this.state.squads.filter((sqd) => sqdIds.includes(sqd.id));
 
     this.isSelectingSquadMove = false;
 
-    units.forEach(squad => {
-      emit(events.LOOKUP_PATH, squad.id, squad.position, vec2(x, y))
-    })
-
-  }
-
+    units.forEach((squad) => {
+      emit(events.LOOKUP_PATH, squad.id, squad.position, vec2(x, y));
+    });
+  };
 }
 
 export default BattlegroundScene;
-
-
-
