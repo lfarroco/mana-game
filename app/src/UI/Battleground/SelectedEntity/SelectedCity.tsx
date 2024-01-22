ccbae00a8647a40182b2b1ee682d500fb57a44a6
Leonardo Farroco <mana-battle@users.noreply.github.com>
2024-01-21 23:06:01 -0300

    Update UI labels and add recruit button for tavern city

--- app/src/UI/Battleground/SelectedEntity/SelectedCity.tsx
+++ app/src/UI/Battleground/SelectedEntity/SelectedCity.tsx
@@ -14,12 +14,17 @@ const SelectedCity = ({ city }: { city: City }) => {
 
 			<h3>{city.name}</h3>
 			<p>{city.type}</p>
+
 		</div>
-		<Button
-			className="col col-2 btn btn-secondary"
-			onClick={emit_(events.TOGGLE_DISPATCH_MODAL, true)}
-		>Recruit</Button>
-		<Button className="col col-2 btn btn-secondary">Shop</Button>
+
+		<div className="col col-4 pt-3">
+			{city.type === "tavern" &&
+				<Button
+					className="btn btn-secondary"
+					onClick={emit_(events.TOGGLE_DISPATCH_MODAL, true)}
+				>Recruit</Button>
+			}
+		</div>
 
 	</div>
 
