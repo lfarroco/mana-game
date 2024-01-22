d05ae94810eb60950b8b4e3c46fa93eeea901262
Leonardo Farroco <leonardo.farroco@gmail.com>
2024-01-22 20:18:48 -0300

    Delete Core.ts and update import path in index.tsx

---
+++ app/src/Scenes/Core/Core.ts
@@ -0,0 +1,39 @@
+import { events, listeners } from "../../Models/Signals";
+import { getState } from "../../Models/State";
+
+// over-complicated phaser type :shrug:
+type PhaserAudio = Phaser.Sound.HTML5AudioSound |
+	Phaser.Sound.NoAudioSound |
+	Phaser.Sound.WebAudioSound
+
+export default class Core extends Phaser.Scene {
+	music: PhaserAudio | null = null
+
+	constructor() {
+		super('CoreScene');
+
+		listeners([
+			[events.PLAY_MUSIC, () => { this.music?.play() }],
+			[events.STOP_MUSIC, () => { this.music?.stop() }],
+		])
+	}
+
+	preload() {
+		this.load.audio('theme', [
+			'assets/audio/main_theme.mp3',
+			'assets/audio/main_theme.ogg',
+			//'jshaw_a_dream_of_first_flight',
+		]);
+	}
+
+	create() {
+
+		this.music = this.sound.add('theme');
+
+		const state = getState()
+
+		if (state.options.music)
+			this.music.play();
+
+	}
+}
