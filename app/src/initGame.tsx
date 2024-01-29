dcf7a22fe7a8ee1c079d67b31f5ee6feb7a7c5b9
Leonardo Farroco <leonardo.farroco@gmail.com>
2024-01-29 00:34:25 -0300

    Update game start event name

---
+++ app/src/initGame.tsx
@@ -0,0 +1,39 @@
+import * as Phaser from "phaser";
+import BattlegroundScene from "./Scenes/Battleground/BattlegroundScene";
+import { events as events_ } from './Models/Signals';
+import { listeners } from './Models/Signals';
+import Core from "./Scenes/Core/Core";
+import * as SaveGame from "./Systems/SaveGame/SaveGame";
+import { State } from "./Models/State";
+import Events from 'events'
+
+export function initGame(state: State, emitter: Events) {
+
+	const game = new Phaser.Game({
+		type: Phaser.AUTO,
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		parent: "game",
+		width: window.innerWidth,
+		height: window.innerHeight,
+		scene: [Core, BattlegroundScene]
+	});
+
+
+	window.addEventListener("resize", () => {
+		game.scale.resize(window.innerWidth, window.innerHeight);
+	});
+
+	listeners([
+		[
+			events_.START_NEW_GAME, () => {
+				// TODO: pass empty gamedata
+				game.scene.start("BattlegroundScene", { ...state.gameData });
+			}
+		],
+	]);
+
+	SaveGame.init(game);
+
+	return game;
+}
