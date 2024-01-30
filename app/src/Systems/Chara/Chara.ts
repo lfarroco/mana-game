3053439d8cf16825ccd1dd3ea4bffef57c888ca7
Leonardo Farroco <mana-battle@users.noreply.github.com>
2024-01-30 13:18:08 -0300

    fixes #10

--- app/src/Systems/Chara/Chara.ts
+++ app/src/Systems/Chara/Chara.ts
@@ -3,7 +3,7 @@ import { Unit } from "../../Models/Unit";
 import { HALF_TILE_HEIGHT, HALF_TILE_WIDTH, TILE_HEIGHT, TILE_WIDTH } from "../../Scenes/Battleground/constants";
 import "./portrait.css"
 import BattlegroundScene from "../../Scenes/Battleground/BattlegroundScene";
-import { BAR_HEIGHT, BAR_WIDTH, BORDER_WIDTH } from "./StaminaBar";
+import { emit, signals } from "../../Models/Signals";
 
 export type Chara = {
 	id: string;
@@ -11,8 +11,6 @@ export type Chara = {
 	job: string;
 	sprite: Phaser.GameObjects.Sprite,
 	emote: Phaser.GameObjects.Sprite | null,
-	staminaBar: Phaser.GameObjects.Graphics | null,
-	staminaBarBackground: Phaser.GameObjects.Graphics | null,
 	group: Phaser.GameObjects.Group | null,
 	movementArrow: Phaser.GameObjects.Sprite | null,
 	movementArrowOverlay: Phaser.GameObjects.Sprite | null
@@ -30,38 +28,9 @@ export function createChara(
 
 	//stamina bar
 
-	const staminaBackground = scene.add.graphics();
-	staminaBackground.fillStyle(0x000000, 1);
-	staminaBackground.fillRect(
-		0,
-		0,
-		BAR_WIDTH,
-		BAR_HEIGHT
-	);
-	const staminaBar = scene.add.graphics();
-	staminaBar.fillStyle(0xffff00, 1);
-	staminaBar.fillRect(
-		0,
-		0,
-		BAR_WIDTH - BORDER_WIDTH * 2,
-		BAR_HEIGHT - BORDER_WIDTH * 2
-	);
 
-	const follow = () => {
-		staminaBackground.x = sprite.x - BAR_WIDTH / 2;
-		staminaBackground.y = sprite.y + HALF_TILE_HEIGHT - BAR_HEIGHT * 2;
-		staminaBar.x = staminaBackground.x + BORDER_WIDTH;
-		staminaBar.y = staminaBackground.y + BORDER_WIDTH
-	};
 
-	//make bars follow sprite
-	scene.events.on("update", follow);
-	//destroy listener on element destroy
-	sprite.once("destroy", () => {
-		scene.events.off("update", follow);
-	});
-
-	const group = scene.add.group([sprite, staminaBackground, staminaBar])
+	const group = scene.add.group([sprite])
 
 	const chara: Chara = {
 		id: squad.id,
@@ -69,8 +38,6 @@ export function createChara(
 		job: squad.job,
 		sprite,
 		emote: null,
-		staminaBar,
-		staminaBarBackground: staminaBackground,
 		group,
 		movementArrow: null,
 		movementArrowOverlay: null
--- app/src/Systems/Chara/StaminaBar.ts
+++ app/src/Systems/Chara/StaminaBar.ts
@@ -1,7 +1,8 @@
 import { listeners, signals } from "../../Models/Signals"
 import { State, getSquad } from "../../Models/State"
+import { Unit } from "../../Models/Unit";
 import BattlegroundScene from "../../Scenes/Battleground/BattlegroundScene"
-import { TILE_WIDTH } from "../../Scenes/Battleground/constants";
+import { HALF_TILE_HEIGHT, TILE_WIDTH } from "../../Scenes/Battleground/constants";
 
 export const CHARA_SCALE = 1;
 export const EMOTE_SCALE = 1;
@@ -10,33 +11,93 @@ export const BAR_HEIGHT = 6;
 export const BORDER_WIDTH = 1;
 
 export function init(state: State, scene: BattlegroundScene) {
+
+	let barIndex: {
+		[id: string]: {
+			bar: Phaser.GameObjects.Graphics,
+			bg: Phaser.GameObjects.Graphics
+		}
+	} = {}
+
 	listeners([
+		[signals.CHARA_CREATED, (id: string) => {
+
+			const chara = scene.getChara(id)
+
+			const squad = getSquad(state)(id)
+
+			const bg = scene.add.graphics();
+			bg.fillStyle(0x000000, 1);
+			bg.fillRect(
+				0,
+				0,
+				BAR_WIDTH,
+				BAR_HEIGHT
+			);
+			const bar = scene.add.graphics();
+			bar.fillStyle(0xffff00, 1);
+			bar.fillRect(
+				0,
+				0,
+				calculateBarWidth(squad.hp, squad.maxHp),
+				BAR_HEIGHT - BORDER_WIDTH * 2
+			);
+
+			const follow = () => {
+				bg.x = chara.sprite.x - BAR_WIDTH / 2;
+				bg.y = chara.sprite.y + HALF_TILE_HEIGHT - BAR_HEIGHT * 2;
+				bar.x = bg.x + BORDER_WIDTH;
+				bar.y = bg.y + BORDER_WIDTH
+			};
+
+			//make bars follow sprite
+			scene.events.on("update", follow);
+			//destroy listener on element destroy
+			chara.sprite.once("destroy", () => {
+				scene.events.off("update", follow);
+			});
+
+			chara.group?.addMultiple([bg, bar])
+
+			barIndex[id] = {
+				bar,
+				bg
+			}
+
+		}],
 		[signals.UPDATE_SQUAD, (id: string, arg: any) => {
 			const { hp } = arg
 
 			// check if hp is defined (it may be 0)
 			if (hp === undefined) return
 
-			const chara = scene.getChara(id)
 			const squad = getSquad(state)(id)
 
-			chara.staminaBar?.clear()
-			chara.staminaBar?.fillStyle(0xffff00, 1);
-			chara.staminaBar?.fillRect(
+			const { bar } = barIndex[id]
+
+			bar.clear()
+			bar.fillStyle(0xffff00, 1);
+			bar.fillRect(
 				0,
 				0,
-				BAR_WIDTH * hp / squad.maxHp - BORDER_WIDTH * 2,
+				calculateBarWidth(hp, squad.maxHp),
 				BAR_HEIGHT - BORDER_WIDTH * 2
 			);
 		}],
 		[signals.SQUAD_DESTROYED, (id: string) => {
 
-			const chara = scene.getChara(id)
+			const { bar, bg } = barIndex[id]
 
-			chara.staminaBar?.destroy()
-			chara.staminaBarBackground?.destroy()
+			bar.destroy()
+			bg.destroy()
+
+			delete barIndex[id]
 
 		}]
 	])
 
+}
+
+function calculateBarWidth(hp: any, maxHp: number): number {
+	return BAR_WIDTH * hp / maxHp - BORDER_WIDTH * 2;
 }
