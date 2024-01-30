301e1b3916f94ec1701d8897614c2243494bae8b
Leonardo Farroco <mana-battle@users.noreply.github.com>
2024-01-30 12:15:37 -0300

    Refactor Chara component and related systems

--- app/src/Systems/Chara/Chara.ts
+++ app/src/Systems/Chara/Chara.ts
@@ -1,9 +1,9 @@
 import Phaser from "phaser";
-import { Unit } from "../Models/Unit";
-import { HALF_TILE_HEIGHT, HALF_TILE_WIDTH, TILE_HEIGHT, TILE_WIDTH } from "../Scenes/Battleground/constants";
+import { Unit } from "../../Models/Unit";
+import { HALF_TILE_HEIGHT, HALF_TILE_WIDTH, TILE_HEIGHT, TILE_WIDTH } from "../../Scenes/Battleground/constants";
 import "./portrait.css"
-import BattlegroundScene from "../Scenes/Battleground/BattlegroundScene";
-import { signals, listeners } from "../Models/Signals";
+import BattlegroundScene from "../../Scenes/Battleground/BattlegroundScene";
+import { signals, listeners } from "../../Models/Signals";
 
 export type Chara = {
 	id: string;
