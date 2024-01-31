9629d318afa6b8492376b14bd2e482ca7b1e389b
Leonardo Farroco <mana-battle@users.noreply.github.com>
2024-01-30 22:09:00 -0300

    Refactor city interaction logic

--- app/src/Scenes/Battleground/Map/makeCitiesInteractive.ts
+++ app/src/Scenes/Battleground/Map/makeCitiesInteractive.ts
@@ -2,37 +2,41 @@ import Phaser from "phaser";
 import { BattlegroundScene } from "../BattlegroundScene";
 import { asVec2 } from "../../../Models/Geometry";
 import { emit, signals } from "../../../Models/Signals";
-import { getState } from "../../../Models/State";
+import { getCity, getState } from "../../../Models/State";
 import { pingAt } from "./Ping";
+import { City } from "../../../Models/City";
 
 export function makeCitiesInteractive(
   scene: BattlegroundScene,
-  cities: Phaser.GameObjects.Image[]
+  cities: { sprite: Phaser.GameObjects.Image, city: City }[]
 ) {
   cities.forEach((city) => {
-    city.setInteractive();
-    city.on(
+    city.sprite.setInteractive();
+    city.sprite.on(
       Phaser.Input.Events.POINTER_UP,
       (pointer: Phaser.Input.Pointer, _x: number, _y: number) => {
         if (pointer.upElement.tagName !== "CANVAS") return;
 
         const state = getState()
 
+
+        if (!scene.isTileVisible(asVec2(city.city.boardPosition))) return
+
         if (
           scene.isSelectingSquadMove ||
           (state.gameData.selectedUnits.length > 0 &&
             pointer.rightButtonReleased())
         ) {
-          const tile = scene.getTileAtWorldXY(asVec2(city));
+          const tile = scene.getTileAtWorldXY(asVec2(city.sprite));
 
           state.gameData.selectedUnits.forEach((unit) => {
             emit(signals.SELECT_SQUAD_MOVE_DONE, unit, asVec2(tile));
           });
 
-          pingAt(scene, city.x, city.y);
+          pingAt(scene, city.sprite.x, city.sprite.y);
 
         } else {
-          emit(signals.CITIES_SELECTED, [city.name]);
+          emit(signals.CITIES_SELECTED, [city.city.id]);
 
           emit(signals.UNITS_SELECTED, []);
         }
