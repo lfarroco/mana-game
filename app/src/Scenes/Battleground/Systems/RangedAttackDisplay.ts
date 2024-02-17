c40b91c23987e81a597aaf8759f49833d6430535
Leonardo Farroco <mana-battle@users.noreply.github.com>
2024-02-17 00:28:02 -0300

    Refactor RangedAttackDisplay module

--- app/src/Scenes/Battleground/Systems/RangedAttackDisplay.ts
+++ app/src/Scenes/Battleground/Systems/RangedAttackDisplay.ts
@@ -1,104 +1,43 @@
-import { Vec2, distanceBetween } from "../../../Models/Geometry";
+import { getDirection } from "../../../Models/Direction";
+import { Vec2, asVec2 } from "../../../Models/Geometry";
 import { getJob } from "../../../Models/Job";
 import { signals, listeners } from "../../../Models/Signals";
-import { State, getState } from "../../../Models/State";
+import { State } from "../../../Models/State";
 import { Unit, isAttacking } from "../../../Models/Unit";
 import BattlegroundScene from "../BattlegroundScene";
+import { HALF_TILE_HEIGHT } from "../constants";
 
+type DisplayIndex = {
+	[key: string]:
+	{
+		targetId: string,
+		graphic: Phaser.GameObjects.Graphics,
+		arrowTip: Phaser.GameObjects.Image,
+		tween: Phaser.Tweens.Tween
+	}
+}
 
 export function init(scene: BattlegroundScene, state: State) {
 
-	let displayIndex: {
-		[key: string]:
-		{
-			targetId: string,
-			graphic: Phaser.GameObjects.Graphics,
-		}
-	} = {}
+	let displayIndex: DisplayIndex = {}
 
 	listeners([
-		[signals.UNITS_SELECTED, (ids: string[]) => {
-
-			ids.forEach(unitId => {
-				const unit = scene.getSquad(unitId);
-				if (isAttacking(unit.status)) {
-					const job = getJob(unit.job);
-					if (job.attackType !== "ranged") return;
-
-					const line = drawLine(scene, unit, unit.status.target, state.options.speed);
-					displayIndex[unitId] = {
-						targetId: unit.status.target,
-						graphic: line
-					}
-				}
-			});
-
-		}],
 		[signals.ATTACK_STARTED, (attacker: string, target: string) => {
 
-			const state = getState()
-
-			if (!state.gameData.selectedUnits.includes(attacker)) return
-
-			const unit = scene.getSquad(attacker)
-
-			const job = getJob(unit.job)
-			if (job.attackType !== "ranged") return
-
-			const line = drawLine(scene, unit, target, state.options.speed)
-
-			displayIndex[attacker] = {
-				targetId: target,
-				graphic: line
-			}
+			renderLine(scene, state, displayIndex, attacker, target);
 		}
 		],
-		[signals.UNITS_DESELECTED, (ids: string[]) => {
-
-			ids.forEach(id => {
-
-				const line = displayIndex[id]
-
-				if (line) {
-					line.graphic.destroy()
-					delete displayIndex[id]
-				}
-
-			})
-		}],
-		[signals.UNIT_DESTROYED, (id: string) => {
-
-			const line = displayIndex[id]
-
-			if (line) {
-				line.graphic.destroy()
-				delete displayIndex[id]
-			}
-
-		}],
 		[signals.UNIT_DESTROYED, (id: string) => {
 
 			// TODO: this could be a problem if the attacker changes its status before the target is destroyed
 
-			Object.entries(displayIndex).forEach(([key, value]) => {
+			cleanUp(displayIndex, id);
 
-				if (value.targetId === id) {
-					value.graphic.destroy()
-					delete displayIndex[key]
-				}
-			})
 		}],
 		[signals.UNIT_LEAVES_CELL, (unitId: string, vec: Vec2) => {
 
-			Object.entries(displayIndex).forEach(([key, value]) => {
 
-				if (value.targetId === unitId) {
-					value.graphic.destroy()
-					delete displayIndex[key]
-				}
-				// TODO: check if new cell is in range
-
-			});
+			cleanUp(displayIndex, unitId);
 
 		}],
 		[signals.UNIT_FINISHED_MOVE_ANIM, (unitId: string, vec: Vec2) => {
@@ -109,32 +48,58 @@ export function init(scene: BattlegroundScene, state: State) {
 
 				if (isAttacking(unit.status)
 					&& unit.status.target === unitId
-					&& state.gameData.selectedUnits.includes(unit.id)
 				) {
 
-
-					const job = getJob(unit.job)
-
-					if (job.attackType !== "ranged") return
-
-					const distance = distanceBetween(unit.position)(vec)
-
-					if (distance > 3) return
-
-					const line = drawLine(scene, scene.getSquad(unit.id), unitId, state.options.speed)
-
-					displayIndex[unit.id] = {
-						targetId: unitId,
-						graphic: line
-					}
+					renderLine(scene, state, displayIndex, unit.id, unitId);
 
 				}
 
 			});
 
 		}],
+		[signals.SELECT_UNIT_MOVE_DONE, (unitId: string) => {
+
+			cleanUp(displayIndex, unitId);
+		}]
 	])
 }
+function renderLine(scene: BattlegroundScene, state: State, displayIndex: DisplayIndex, attacker: string, target: string) {
+	const unit = scene.getSquad(attacker)
+
+	const job = getJob(unit.job)
+	if (job.attackType !== "ranged") return
+
+
+	const { graphics, arrowTip, tween } = drawLine(scene, unit, target, state.options.speed);
+
+	displayIndex[attacker] = {
+		targetId: target,
+		graphic: graphics,
+		arrowTip,
+		tween
+	};
+}
+
+function cleanUp(
+
+	displayIndex: {
+		[key: string]: {
+			targetId: string; graphic: Phaser.GameObjects.Graphics;
+			arrowTip: Phaser.GameObjects.Image;
+			tween: Phaser.Tweens.Tween;
+		};
+	},
+	key: string) {
+
+	const value = displayIndex[key];
+	if (!value) return;
+	value.graphic.destroy();
+	value.arrowTip.destroy();
+	value.tween.stop()
+	//value.tween.destroy()
+	delete displayIndex[key];
+}
+
 function drawLine(scene: BattlegroundScene, unit: Unit, targetId: string, speed: number) {
 
 	const source = scene.getChara(unit.id)
@@ -150,28 +115,54 @@ function drawLine(scene: BattlegroundScene, unit: Unit, targetId: string, speed:
 		Math.ceil(distance / 10)
 	)
 
-	graphics.lineStyle(1, 0x00ff00, 1)
+	const arrowTip = scene.add.image(target.sprite.x, target.sprite.y, "arrow-top-emote")
+
+	arrowTip.setScale(0.3).setAlpha(0.5).setTint(0xff0000)
+	const direction = getDirection(asVec2(source.sprite), asVec2(target.sprite))
+
+	arrowTip.setAngle({
+		up: 0,
+		down: 180,
+		left: 270,
+		right: 90
+	}[direction])
+
 
 	points.forEach((point, i) => {
 
 		scene.time.addEvent({
-			delay: 20 * i,
+			delay: 30 * i,
 			callback: () => {
 
 				if (graphics.active && point.x && point.y) {
 
-					graphics.lineStyle(4, 0xff0000, 0.8)
+					const start = points[i - 1]
+
+					graphics.lineStyle(5, 0xff0000, 0.3)
 					graphics.beginPath()
-					graphics.moveTo(source.sprite.x, source.sprite.y)
-					graphics.lineTo(point.x, point.y)
+					graphics.moveTo(
+						start?.x || source.sprite.x,
+						(start?.y || source.sprite.y) + HALF_TILE_HEIGHT - 15,
+					)
+					graphics.lineTo(point.x, point.y + HALF_TILE_HEIGHT - 15)
 					graphics.closePath()
 					graphics.strokePath()
+					arrowTip.setPosition(point.x, point.y + HALF_TILE_HEIGHT - 15)
 
 				}
 			}
 		})
 	})
 
-	return graphics
+	const tween = scene.add.tween({
+
+		targets: [graphics, arrowTip],
+		alpha: 0,
+		duration: 666,
+		ease: "Cubic",
+		repeat: -1
+	})
+
+	return { graphics, arrowTip, tween }
 
 }
