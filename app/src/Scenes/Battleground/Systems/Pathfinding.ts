c6426e358420c38f03a0709d210293835da995ab
Leonardo Farroco <leonardo.farroco@nextroll.com>
2025-01-11 11:23:28 -0300

    remove status changes on movement

--- app/src/Scenes/Battleground/Systems/Pathfinding.ts
+++ app/src/Scenes/Battleground/Systems/Pathfinding.ts
@@ -2,8 +2,6 @@ import * as Easystar from "easystarjs";
 import { Vec2, asVec2, eqVec2 } from "../../../Models/Geometry";
 import { emit, signals, listeners } from "../../../Models/Signals";
 import { getUnit, getState } from "../../../Models/State";
-import { UNIT_STATUS, isAttacking, isMoving } from "../../../Models/Unit";
-import { getDirection } from "../../../Models/Direction";
 import BattlegroundScene from "../BattlegroundScene";
 
 export function init(scene: BattlegroundScene) {
@@ -54,33 +52,12 @@ export function init(scene: BattlegroundScene) {
     [
       signals.PATH_FOUND,
       (unitId: string, path: Vec2[]) => {
+        // TODO: move this to chara system?
         const state = getState();
 
         const unit = getUnit(state)(unitId)
 
-        // in case of choosing own cell
-        if (path.length === 0) {
-          emit(signals.UPDATE_UNIT, unit.id, { path: [] });
-          if (isMoving(unit.status)) {
-            emit(signals.UPDATE_UNIT, unit.id, { status: UNIT_STATUS.IDLE() });
-          }
-
-          return;
-        } else {
-          emit(signals.UPDATE_UNIT, unit.id, { path });
-          const direction = getDirection(unit.position, path[0]);
-          emit(signals.FACE_DIRECTION, unit.id, direction);
-
-          if (isAttacking(unit.status)) { //TODO: is there an event to exit combat?
-            //emit(signals.REMOVE_EMOTE, unit.id, "combat-emote")
-            emit(signals.COMBAT_FINISHED, unit.id);
-          }
-
-          emit(signals.UPDATE_UNIT, unit.id, {
-            status: UNIT_STATUS.MOVING(path[path.length - 1]),
-          });
-
-        }
+        emit(signals.UPDATE_UNIT, unit.id, { path });
       },
     ],
   ]);
