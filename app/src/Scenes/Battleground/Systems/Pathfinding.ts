fd2e09b59531ca226bb6a25322da44d9c9b04fcc
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-01-19 22:53:00 -0300

    refactor pathfinding and popText usage for improved AI movement and message display

--- app/src/Scenes/Battleground/Systems/Pathfinding.ts
+++ app/src/Scenes/Battleground/Systems/Pathfinding.ts
@@ -3,6 +3,8 @@ import { Vec2, asVec2, } from "../../../Models/Geometry";
 import { emit, signals, listeners } from "../../../Models/Signals";
 import { getUnit, getState } from "../../../Models/State";
 import BattlegroundScene from "../BattlegroundScene";
+import { FORCE_ID_CPU } from "../../../Models/Force";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 export function init(scene: BattlegroundScene) {
   listeners([
@@ -25,23 +27,46 @@ export function init(scene: BattlegroundScene) {
   ]);
 }
 
-export async function lookupPath(scene: BattlegroundScene, unitId: string, source: Vec2, target: Vec2) {
+/**
+ * Function used by the AI to find a path to a target
+ * Avoids allied units
+ */
+export async function lookupAIPAth(
+  scene: BattlegroundScene,
+  unitId: string,
+  source: Vec2,
+  target: Vec2,
+) {
 
-  return new Promise<Vec2[]>((resolve, reject) => {
+  return new Promise<Vec2[]>(async (resolve, reject) => {
     const easystar = new Easystar.js();
     easystar.setAcceptableTiles([0]);
     easystar.setGrid(scene.grid);
 
+    // set tiles with units as blocked
+    const state = getState();
+    state.gameData
+      .units
+      .filter(u => u.force === FORCE_ID_CPU)
+      .forEach(unit => {
+        const { x, y } = unit.position;
+        easystar.avoidAdditionalPoint(x, y);
+      });
+
     easystar.findPath(source.x, source.y, target.x, target.y, (path) => {
-      if (!path) return;
+      if (!path) {
+        resolve([]);
+        return;
+      }
+
 
       const path_ = path.map(asVec2).slice(1)
 
-      emit(signals.PATH_FOUND, unitId, path_);
 
       resolve(path_)
     });
 
     easystar.calculate();
+
   })
 }
