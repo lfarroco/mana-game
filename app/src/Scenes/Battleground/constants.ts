3ef3979ea2c78a78c86302039ebbc76c8cc64698
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-02-25 21:44:08 -0300

    feat: define screen dimensions and update game initialization to use constants

--- app/src/Scenes/Battleground/constants.ts
+++ app/src/Scenes/Battleground/constants.ts
@@ -1,4 +1,8 @@
 
+export const SCREEN_WIDTH = 1280;
+export const SCREEN_HEIGHT = 768;
+
+
 export const TILE_WIDTH = 64;
 export const TILE_HEIGHT = 64;
 export const HALF_TILE_WIDTH = TILE_WIDTH / 2;
