861a408a06509aebfa62147cb1ffb7bc6737d442
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-03-07 23:01:03 -0300

    feat: implement critical damage display and impact effect for slash skill; remove obsolete slash animation

---
+++ app/src/Effects/criticalDamageDisplay.ts
@@ -0,0 +1,45 @@
+import BattlegroundScene from "../Scenes/Battleground/BattlegroundScene";
+import { tween } from "../Utils/animation";
+
+export async function criticalDamageDisplay(
+	scene: BattlegroundScene,
+	{ x, y }: { x: number; y: number; },
+	damage: number,
+	speed: number) {
+	const critBg = scene.add.image(
+		x, y,
+		'damage_display'
+	);
+	critBg.setScale(0);
+
+
+	const dmg = scene.add.text(
+		x, y,
+		`${damage}`,
+		{
+			fontSize: '96px',
+			color: '#000000',
+			stroke: '#000000',
+			strokeThickness: 4,
+			align: 'center',
+			fontStyle: 'bold',
+		}
+	);
+	dmg.setOrigin(0.5);
+	dmg.setScale(0);
+
+	await tween({
+		targets: [critBg, dmg],
+		scale: 0.4,
+		duration: 500 / speed,
+		ease: 'Bounce.easeOut',
+	});
+
+	await tween({
+		targets: [critBg, dmg],
+		alpha: 0,
+		duration: 500 / speed,
+	});
+
+	[critBg, dmg].forEach(fx => fx.destroy());
+}
--- app/src/Systems/Chara/Skills/slash.ts
+++ app/src/Systems/Chara/Skills/slash.ts
@@ -2,8 +2,11 @@ import { getUnit } from "../../../Models/State";
 import { Unit, unitLog } from "../../../Models/Unit";
 import { bashPieceAnimation } from "../Animations/bashPieceAnimation";
 import { popText } from "../Animations/popText";
-import { slashAnimation } from "../Animations/slashAnimation";
 import BattlegroundScene from "../../../Scenes/Battleground/BattlegroundScene";
+import { delay, tween } from "../../../Utils/animation";
+import { emit, signals } from "../../../Models/Signals";
+import { impactEffect } from "../../../Effects/impactEffect";
+import { criticalDamageDisplay } from "../../../Effects/criticalDamageDisplay";
 
 export async function slash(
 	scene: BattlegroundScene,
@@ -11,22 +14,73 @@ export async function slash(
 	target: Unit,
 ) {
 
-	await popText(scene, "Slash", unit.id);
+	const state = scene.state;
+	const { speed } = state.options
 
 	const activeChara = scene.getChara(unit.id);
-
 	const targetUnit = getUnit(scene.state)(target.id);
-
 	const targetChara = scene.getChara(targetUnit.id);
 
 	if (targetUnit.hp <= 0) {
 		throw new Error("target is dead");
 	}
 
+	await popText(scene, "Slash", unit.id);
+
 	unitLog(unit, `will cast slash on ${targetUnit.id}`);
 
 	bashPieceAnimation(activeChara, targetChara);
 
-	await slashAnimation(scene, activeChara, targetChara);
+	scene.playFx("audio/sword2");
+
+	await delay(scene, 300 / speed);
+
+	// TODO: replace with function that checks for dodge
+	// it will check for other factors including buffs
+	const dodges = targetChara.unit.statuses["next-dodge"] > 0;
+
+	if (dodges) {
+		popText(scene, "Dodge", targetChara.unit.id);
+		delete targetChara.unit.statuses["next-dodge"];
+		return;
+	}
+
+	impactEffect({
+		scene,
+		location: targetChara.container,
+		pointA: activeChara.container,
+		pointB: targetChara.container,
+		speed,
+	});
+
+	// TODO: replace with function that checks for critical
+	// it will check for other factors including buffs
+	const isCritical = activeChara.unit.statuses["next-critical"] > 0;
+
+	const damage = isCritical ? activeChara.unit.attack * 3 : activeChara.unit.attack;
+
+	if (isCritical) {
+		criticalDamageDisplay(scene, targetChara.container, damage, speed);
+
+		activeChara.unit.statuses["next-critical"] = 0;
+
+	} else {
+
+		popText(scene, damage.toString(), targetChara.unit.id);
+	}
+
+	tween({
+		targets: [targetChara.container],
+		alpha: 0.5,
+		duration: 100 / speed,
+		yoyo: true,
+		repeat: 4,
+	});
+
+	emit(
+		signals.DAMAGE_UNIT,
+		targetChara.id,
+		damage
+	);
 
 }
