774c2d4b036601b51abab782b630dbb9f880fb04
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-03-26 23:11:29 -0300

    feat: enhance skill icon display in character creation with improved borders and positioning

--- app/src/Systems/Chara/Chara.ts
+++ app/src/Systems/Chara/Chara.ts
@@ -64,19 +64,28 @@ export function createChara(unit: Unit): Chara {
 		container,
 		unit,
 		hightlightTween: null,
-	}
+	};
 
 	unit.learnedSkills.forEach((skillId, i) => {
 
 		const skill = getSkill(skillId);
 
-		const skillBorder = scene.add.rectangle(0, 0, 48, 48, 0xff0000, 1).setOrigin(0.5, 0.5);
-		skillBorder.setPosition(-spriteSize / 2 + 20, spriteSize / 2 - 20);
+		const iconSize = 40;
+		const borderWidth = 2;
+		const borderColor = 0xeaeaea;
+
+		const x = i * iconSize + i * 5;
+
+		const skillBorder = scene.add.rectangle(0, 0, iconSize + borderWidth, iconSize + borderWidth, borderColor, 1)
+			.setOrigin(0.5, 0.5);
+		skillBorder.setPosition(-spriteSize / 2 + iconSize / 2 + x, spriteSize / 2 - iconSize / 2);
 		container.add(skillBorder);
 
-		const skillIcon = scene.add.image(0, 0, skill.pic).setDisplaySize(40, 40).setOrigin(0.5, 0.5);
+		const skillIcon = scene.add.image(0, 0, skill.pic)
+			.setDisplaySize(iconSize, iconSize)
+			.setOrigin(0.5, 0.5);
 		//round mask cutting the skill icon
-		skillIcon.setPosition(-spriteSize / 2 + 20, spriteSize / 2 - 20);
+		skillIcon.setPosition(-spriteSize / 2 + iconSize / 2 + x, spriteSize / 2 - iconSize / 2);
 		container.add(skillIcon);
 
 	});
