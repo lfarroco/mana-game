32c34e9cb77372ac988e7fcc39539da7019ec16a
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-03-28 23:35:58 -0300

    feat: implement character destruction and enhance pop-up text animations for unit status updates

--- app/src/Systems/Chara/Animations/popText.ts
+++ app/src/Systems/Chara/Animations/popText.ts
@@ -4,14 +4,18 @@ import * as UnitManager from "../../../Scenes/Battleground/Systems/UnitManager";
 import { getState } from "../../../Models/State";
 
 // TODO: add color option (heals: green, damage: yellow, etc)
-export async function popText(scene: Phaser.Scene, text: string, targetId: string) {
+export async function popText(scene: Phaser.Scene, text: string, targetId: string, type?: string) {
+
+	const color = type === "heal" ? "#00FF00" : type === "damage" ? defaultTextConfig.color : undefined;
 
 	const chara = UnitManager.getChara(targetId);
 	const popText = scene.add.text(
 		chara.container.x, chara.container.y,
 		text,
 		defaultTextConfig
-	).setOrigin(0.5, 0.5);
+	)
+		.setOrigin(0.5, 0.5)
+		.setColor(color || defaultTextConfig.color)
 
 	await tween({
 		targets: [popText],
