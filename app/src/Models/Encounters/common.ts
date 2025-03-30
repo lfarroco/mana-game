efc5765428541cd0a77ad7c5054c59f17e23cc91
Leonardo Farroco <leonardo.farroco@nextroll.com>
2025-03-30 16:08:31 -0300

    feat: implement encounter system with monster and common events

---
+++ app/src/Models/Encounters/common.ts
@@ -0,0 +1,103 @@
+import { Choice, newChoice } from "../../Scenes/Battleground/Systems/Choice";
+import * as UIManager from "../../Scenes/Battleground/Systems/UIManager";
+import { popText } from "../../Systems/Chara/Animations/popText";
+import { pickRandom } from "../../utils";
+import { FORCE_ID_PLAYER } from "../Force";
+import { BLOB, jobs } from "../Job";
+import { emit, signals } from "../Signals";
+import { getPlayerForce, State } from "../State";
+import { Unit } from "../Unit";
+import { Encounter, makeEncounter, TIER } from "./Encounter";
+
+const commonEvents = (): Encounter[] => [
+	makeEncounter("odd_job", TIER.COMMON, "Odd Job", "You have been offered a job for 5 gold", "icon/job_contract", {
+		type: "instant",
+		action: (_scene, state: State) => {
+			const playerForce = getPlayerForce(state);
+			playerForce.gold += 5;
+			UIManager.updateUI();
+		}
+	}),
+	makeEncounter("new_friend", TIER.COMMON, "A new friend", "You have made a new friend", "icon/quest", {
+		type: "instant",
+		action: (scene, state) => {
+			emit(signals.ADD_UNIT_TO_GUILD, FORCE_ID_PLAYER, BLOB);
+		}
+	}),
+	makeEncounter("learn_something_new", TIER.COMMON, "Learn something new", "A guild member has learned something new", "icon/merchant", {
+		type: "shop", choices: () => {
+			return [
+				newChoice("learn", "Learn a New Skill", "Pay 5 gold to learn a new skill", "learn"),
+				newChoice("study", "Study the Ancient Texts", "Spend 10 gold to gain knowledge", "study"),
+				newChoice("research", "Conduct Research", "Invest 15 gold in research for future benefits", "research")
+			]
+		}
+	}),
+	makeEncounter("endurance_training", TIER.COMMON, "Endurance Training", "Make a guild member gain 30 HP", "icon/endurance_training", {
+		type: "unit",
+		onChoose: async (scene: Phaser.Scene, state, unit: Unit) => {
+
+			unit.maxHp += 30;
+			unit.hp = unit.maxHp;
+
+			popText(scene, "+30 MAX HP", unit.id, "heal");
+		}
+	}),
+	makeEncounter("treasure_hunt", TIER.COMMON, "Treasure Hunt", "You have found a treasure chest", "icon/hidden_treasure", {
+		type: "instant",
+		action: (scene, state: State) => {
+			const playerForce = getPlayerForce(state);
+			playerForce.gold += 20;
+			UIManager.updateUI();
+		}
+	}),
+	makeEncounter("investment_opportunity", TIER.COMMON, "Investment Opportunity", "A merchant offers you a chance to invest in their business, increasing your guild's income.", "icon/quest", {
+		type: "instant",
+		action: (scene, state: State) => {
+			const playerForce = getPlayerForce(state);
+			playerForce.income += 2;
+			UIManager.updateUI();
+		}
+	}),
+	makeEncounter("the_tavern", TIER.COMMON, "The Tavern", "A rowdy tavern full of potential recruits and rumors", "icon/quest", {
+		type: "nested",
+		choices: () => {
+			return [
+				newChoice("tavern", "Hire Mercenary", "Pay 15 gold to recruit a seasoned warrior", "mercenary"),
+				newChoice("tavern", "Buy a Round of Drinks", "Spend 5 gold to hear valuable rumors", "rumors"),
+				newChoice("tavern", "Join the Card Game", "Gamble 8 gold for a chance to win more", "gamble")
+			]
+		},
+		onChoose: (state: State, choice: Choice) => {
+			const playerForce = state.gameData.forces.find(f => f.id === FORCE_ID_PLAYER)!;
+			if (choice.value === "mercenary" && playerForce.gold >= 15) {
+				playerForce.gold -= 15;
+				const randomJob = pickRandom(jobs, 1)[0];
+				// TODO: limit tier
+				emit(signals.ADD_UNIT_TO_GUILD, FORCE_ID_PLAYER, randomJob.id);
+				UIManager.updateUI();
+			} else if (choice.value === "rumors" && playerForce.gold >= 5) {
+				playerForce.gold -= 5;
+				playerForce.income += 1;
+				UIManager.updateUI();
+			} else if (choice.value === "gamble" && playerForce.gold >= 8) {
+				playerForce.gold -= 8;
+				if (Math.random() < 0.4) {
+					playerForce.gold += 20;
+				}
+				UIManager.updateUI();
+			}
+		}
+	}),
+	makeEncounter("lost_treasure", TIER.COMMON, "Lost Treasure", "You stumble upon a hidden chest in the forest", "icon/hidden_treasure", {
+		type: "instant",
+		action: (scene, state: State) => {
+			const playerForce = getPlayerForce(state);
+			playerForce.gold += 10;
+			UIManager.updateUI();
+		}
+	}),
+
+];
+
+export default commonEvents;
