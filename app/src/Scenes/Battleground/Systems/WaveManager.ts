2d5f125c750f3e55c7a047136b228f9765f4aece
Leonardo Farroco <leonardo.farroco@nextroll.com>
2025-04-03 10:03:41 -0300

    feat: refactor wave creation to utilize runUnitHandlers for onBattleStart traits and add trait documentation

--- app/src/Scenes/Battleground/Systems/WaveManager.ts
+++ app/src/Scenes/Battleground/Systems/WaveManager.ts
@@ -2,15 +2,13 @@ import { waves } from "../enemyWaves";
 import { BattlegroundScene } from "../BattlegroundScene";
 import * as UnitManager from "./UnitManager";
 import processTick from "../ProcessTick";
-import { getTrait } from "../../../Models/Traits";
-import { runPromisesInOrder } from "../../../utils";
+import { runUnitHandlers } from "../../../Models/Traits";
 
-let scene: BattlegroundScene;
+export let scene: BattlegroundScene;
 
 export function init(sceneRef: BattlegroundScene) {
 	scene = sceneRef;
 }
-
 export async function createWave(id: number) {
 	const enemies = waves[id];
 
@@ -22,19 +20,7 @@ export async function createWave(id: number) {
 
 	scene.state.battleData.units.forEach(UnitManager.renderChara);
 
-	const promises = scene.state.battleData.units.flatMap((unit, i) => {
-		return unit.traits.reduce((xs, traitId) => {
-			const trait = getTrait(traitId);
-
-			if (trait.onBattleStart) {
-				return xs.concat([async () => await trait.onBattleStart!(unit)]);
-			} else {
-				return xs
-			}
-		}, [] as (() => Promise<void>)[]);
-	});
-
-	await runPromisesInOrder(promises)
+	await runUnitHandlers('onBattleStart');
 
 	await processTick(scene);
 
