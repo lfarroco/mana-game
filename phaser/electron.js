63e3260dd7ab1937e683088b77d66f8279d71370
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-05-16 10:43:09 -0300

    refactor: Update arcaneMissiles to use getRangedTargets and improve target selection logic

--- phaser/electron.js
+++ phaser/electron.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 
+// continue from here https://www.electronjs.org/docs/latest/tutorial/tutorial-first-app
+
 function createWindow() {
 	const mainWindow = new BrowserWindow(/* your options */);
 	mainWindow.loadFile('dist/index.html');
