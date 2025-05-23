8f92ce374b5975013c013dd027bcc90235c97232
Leonardo Farroco <leonardo.farroco@nextroll.com>
2025-05-23 13:19:34 -0300

    feat: Refactor enemy team generation logic and integrate into BattlegroundScene

--- phaser/src/Scenes/Battleground/BattlegroundScene.ts
+++ phaser/src/Scenes/Battleground/BattlegroundScene.ts
@@ -15,13 +15,13 @@ import * as EventSystem from "../../Models/Encounters/Encounter";
 import * as TraitSystem from "../../Models/Traits";
 import * as TooltipSystem from "../../Systems/Tooltip";
 import { makeUnit } from "../../Models/Unit";
-import { cpuForce, playerForce } from "../../Models/Force";
-import { ARCHER, CLERIC, cards, KNIGHT, Card } from "../../Models/Card";
+import { playerForce } from "../../Models/Force";
+import { ARCHER, CLERIC, KNIGHT } from "../../Models/Card";
 import { vec2 } from "../../Models/Geometry";
 import runCombatIO from "./RunCombatIO";
-import { pickOne } from "../../utils";
 import { battleResultAnimation } from "./battleResultAnimation";
 import { delay } from "../../Utils/animation";
+import { generateEnemyTeam } from "./generateEnemyTeam";
 
 export class BattlegroundScene extends Phaser.Scene {
 
@@ -137,7 +137,7 @@ export class BattlegroundScene extends Phaser.Scene {
       while (state.gameData.hour < 9) {
         state.battleData.units = [];
 
-        generateEnemyTeam(state);
+        generateEnemyTeam(state, state.gameData.player.units.length);
         state.battleData.units = [...state.battleData.units, ...state.gameData.player.units];
 
         UnitManager.clearCharas();
@@ -212,160 +212,4 @@ export class BattlegroundScene extends Phaser.Scene {
 
 export default BattlegroundScene;
 
-function generateEnemyTeam(state: State) {
 
-  // t = tank
-  // r = ranged dps
-  // s = support
-  // m = melee dps
-  const templates: { [hour: number]: string[] } = {
-    3: [
-      `
-    xxx
-    rst
-    xxx
-    `,
-      `
-    xxm
-    xsx
-    xxm
-    `,
-      `
-    xxx
-    xxt
-    rxm
-    `
-    ],
-    4: [
-      `
-      rxm
-      xxx
-      rxm
-      `,
-      `
-      xxx
-      rsm
-      xxm
-      `,
-      `
-      xxm
-      sxm
-      xxm
-      `,
-      `
-      rxx
-      rxm
-      rxx
-      `
-    ],
-    5: [
-      `
-      rxm
-      xxt
-      rxm
-      `,
-      `
-      rxt
-      rst
-      xxx
-      `,
-      `
-      xxm
-      rst
-      xxm
-      `,
-    ],
-    6: [
-      `
-      rxm
-      rxm
-      rxm
-      `,
-      `
-      sxm
-      xxt
-      sxm
-      `,
-    ],
-    7: [
-      `
-      rxm
-      rsm
-      rxm
-      `,
-      `
-      rxt
-      rsm
-      rxt
-      `,
-      `
-      sxt
-      rsm
-      sxt
-      ` ,
-    ],
-    8: [
-      `
-      rxt
-      rsm
-      rst
-      `,
-      `
-      srt
-      sxt
-      srt
-      `,
-    ],
-    9: [
-      `
-      rst
-      rst
-      rst
-      `,
-      `
-      rrm
-      sst
-      rrm
-      `,
-    ],
-  }
-
-  const template = pickOne(templates[state.gameData.hour + 2]);
-
-  const parsed = template.split("\n")
-    .filter(line => line.trim() !== "")
-    .map(line => line.trim().split(""));
-
-  const getRanged = () => cards.filter(c => c.traits.includes(TraitSystem.RANGED.id))
-  const getMelee = () => cards.filter(c => c.traits.includes(TraitSystem.MELEE.id))
-  const getSupport = () => cards.filter(c => c.traits.includes(TraitSystem.SUPPORT.id))
-  const getTank = () => cards.filter(c => c.traits.includes(TraitSystem.TAUNT.id))
-
-  for (let y = 0; y < parsed.length; y++) {
-    const row = parsed[y];
-    for (let x = 0; x < row.length; x++) {
-      const char = row[x];
-      let card: Card | undefined;
-      switch (char) {
-        case "r":
-          card = pickOne(getRanged());
-          break;
-        case "m":
-          card = pickOne(getMelee());
-          break;
-        case "s":
-          card = pickOne(getSupport());
-          break;
-        case "t":
-          card = pickOne(getTank());
-          break;
-        default:
-          break;
-      }
-      if (card !== undefined) {
-        state.battleData.units.push(makeUnit(cpuForce.id, card.id, vec2(x + 1, y + 1)));
-      }
-    }
-  }
-
-}
