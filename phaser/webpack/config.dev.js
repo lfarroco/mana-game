c303885251afe368562505ce49242bab8e4f865c
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-05-24 11:42:20 -0300

    Refactor code structure for improved readability and maintainability

--- phaser/webpack/config.dev.js
+++ phaser/webpack/config.dev.js
@@ -2,6 +2,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -55,6 +56,11 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: "./index.html"
-        })
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "src/data", to: "assets/data" }
+            ]
+        }),
     ]
 };
