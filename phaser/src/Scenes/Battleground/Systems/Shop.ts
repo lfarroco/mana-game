760fcfee15ef6352dbfe2fcada2a835ed77e3287
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-06-05 22:26:45 -0300

    format shop

--- phaser/src/Scenes/Battleground/Systems/Shop.ts
+++ phaser/src/Scenes/Battleground/Systems/Shop.ts
@@ -1,7 +1,9 @@
+import { images } from "../../../assets";
 import { getEmptySlot } from "../../../Models/Board";
 import { getAllCards } from "../../../Models/Card";
 import { playerForce, updatePlayerGoldIO } from "../../../Models/Force";
 import { vec2 } from "../../../Models/Geometry";
+import { State } from "../../../Models/State";
 import { makeUnit } from "../../../Models/Unit";
 import { addBoardEvents, addTooltip, createCard } from "../../../Systems/Chara/Chara";
 import { Flyout } from "../../../Systems/Flyout";
@@ -9,30 +11,92 @@ import * as Tooltip from "../../../Systems/Tooltip";
 import { pickRandom } from "../../../utils";
 import { tween } from "../../../Utils/animation";
 import BattlegroundScene from "../BattlegroundScene";
-import { FORCE_ID_PLAYER, MAX_PARTY_SIZE, SCREEN_WIDTH } from "../constants";
+import { FORCE_ID_PLAYER, MAX_PARTY_SIZE, SCREEN_WIDTH, titleTextConfig } from "../constants";
 import { getCharaPosition } from "./CharaManager";
 import { createButton, displayError } from "./UIManager";
 
 export const open = (scene: BattlegroundScene) => new Promise<void>(async (resolve) => {
 
 	const { state } = scene;
-	const flyout = new Flyout(scene, "Shop");
+	const flyout = new Flyout(scene, "");
 
-	const filtered =
-		getAllCards().filter(card =>
-			!state.gameData.player.units.map(u => u.job).includes(card.name)
+	relics(scene, flyout);
+
+	tavern(state, flyout);
+
+	const nextRoundBtn = createButton(
+		"Next Round",
+		SCREEN_WIDTH - 180,
+		500,
+		async () => {
+			await flyout.slideOut();
+			flyout.destroy();
+			resolve();
+		}
+	)
+	flyout.add(nextRoundBtn);
+
+	flyout.slideIn();
+
+});
+
+function relics(scene: BattlegroundScene, flyout: Flyout) {
+	const relics = [
+		images.arrow.key, images.agility_training.key, images.chest.key
+	];
+
+	const bg = scene.add.graphics()
+		.fillStyle(0x000, 0.5)
+		.fillRect(0, 0, 700, 400)
+		.setPosition(50, 50);
+
+	const title = scene.add.text(300, 70, "Relics", titleTextConfig);
+	flyout.add([bg, title]);
+
+	relics.forEach((relic, index) => {
+
+		const x = index * 210 + 190;
+		const y = 300;
+
+		const iconSize = 200;
+
+		const slot = scene.add
+			.image(x, y, images.slot.key)
+			.setOrigin(0.5)
+			.setDisplaySize(iconSize, iconSize);
+		const icon = scene.add
+			.image(x, y, relic)
+			.setOrigin(0.5)
+			.setDisplaySize(iconSize - 40, iconSize - 40);
+
+		flyout.add([slot, icon]);
+	});
+}
+
+function tavern(state: State, flyout: Flyout) {
+
+	const bg = flyout.scene.add.graphics()
+		.fillStyle(0x000, 0.5)
+		.fillRect(800, 0, 600, 400)
+		.setPosition(50, 50);
+
+	const title = flyout.scene.add.text(900, 60, "Tavern", titleTextConfig);
+	flyout.add([bg, title]);
+
+	const filtered = getAllCards()
+		.filter(card => !state.gameData.player.units.map(u => u.job).includes(card.name)
 		);
 
-	pickRandom(filtered, 5)
+	pickRandom(filtered, 3)
 		.forEach((spec, index) => {
 			const unit = makeUnit(FORCE_ID_PLAYER, spec.name, vec2(0, 0));
 			const card = createCard(unit);
 
-			card.container.setPosition(750 + index * 200, 250);
+			card.container.setPosition(950 + index * 200, 300);
 
 			card.zone.setInteractive({ draggable: true });
 
-			addTooltip(card)
+			addTooltip(card);
 
 			// TODO: replace with drag and drop
 			card.zone.on("pointerup", () => {
@@ -59,7 +123,7 @@ export const open = (scene: BattlegroundScene) => new Promise<void>(async (resol
 				card.unit.position = emptySlot;
 				state.gameData.player.units.push(card.unit);
 
-				const pos = getCharaPosition(card.unit)
+				const pos = getCharaPosition(card.unit);
 				tween({
 					targets: [card.container],
 					...pos,
@@ -72,19 +136,5 @@ export const open = (scene: BattlegroundScene) => new Promise<void>(async (resol
 
 			flyout.add(card.container);
 		});
+}
 
-	const nextRoundBtn = createButton(
-		"Next Round",
-		SCREEN_WIDTH - 180,
-		500,
-		async () => {
-			await flyout.slideOut();
-			flyout.destroy();
-			resolve();
-		}
-	)
-	flyout.add(nextRoundBtn);
-
-	flyout.slideIn();
-
-});
