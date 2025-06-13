4c490626b4e77be57930f4f56e2576395288e51c
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-06-13 17:10:20 -0300

    update globals

--- phaser/e2e/types.ts
+++
@@ -1,8 +0,0 @@
-import { DebugController } from '../src/Scenes/Debug/DebugController';
-
-declare global {
-	interface Window {
-		//game: Phaser.Game; // already declared in main project, kept here for reference
-		gameController: DebugController;
-	}
-}
--- phaser/src/main.ts
+++ phaser/src/main.ts
@@ -1,11 +1,5 @@
-import { State, initialState } from "./Models/State";
+import { initialState } from "./Models/State";
 import { initGame } from "./initGame";
-
-declare global {
-  interface Window {
-    state: State; // TODO: this is bad
-  }
-}
 
 const state = initialState();
 window.state = state;
