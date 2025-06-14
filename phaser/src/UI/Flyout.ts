da3523c667ba96ee925b60df82bb4c73fe4906c2
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-06-14 20:51:05 -0300

    refactor: adjust shop UI dimensions and simplify flyout initialization

--- phaser/src/UI/Flyout.ts
+++ phaser/src/UI/Flyout.ts
@@ -1,45 +1,24 @@
-import { images } from "../assets";
 import BattlegroundScene from "../Scenes/Battleground/BattlegroundScene";
-import { defaultTextConfig, SCREEN_HEIGHT, SCREEN_WIDTH, TILE_HEIGHT, titleTextConfig } from "../constants/constants";
+import { SCREEN_HEIGHT } from "../constants/constants";
 import { tween } from "../Utils/animation";
 
-const flyoutWidth = SCREEN_WIDTH;
-const flyoutHeight = SCREEN_HEIGHT / 1.5;
 
 let flyouts: Flyout[] = [];
-
-//@ts-ignore
-window.flyouts = flyouts;
 
 export class Flyout extends Phaser.GameObjects.Container {
 
 	isOpen = false;
-	bg: Phaser.GameObjects.Graphics;
-	titleText: Phaser.GameObjects.Text;
 
-	constructor(public scene: BattlegroundScene, title: string) {
+	constructor(public scene: BattlegroundScene) {
 		super(scene);
-		this.setName(title);
 		scene.add.existing(this);
 		flyouts.push(this);
 
-		this.bg = scene.add.graphics()
-			.fillStyle(0x666666, 0.8)
-			.fillRect(0, 0, flyoutWidth, flyoutHeight)
-			.setPosition(0, 0);
 
-		this.bg.setInteractive();
 
-		this.titleText = scene.add.text(
-			SCREEN_WIDTH / 2,
-			TILE_HEIGHT / 2,
-			title,
-			titleTextConfig,
-		).setOrigin(0.5);
 
-		this.add([this.bg, this.titleText]);
 
-		this.setY(-flyoutHeight);
+		this.setY(-SCREEN_HEIGHT);
 
 		this.on("destroy", () => {
 			flyouts = flyouts.filter(f => f !== this);
@@ -62,33 +41,9 @@ export class Flyout extends Phaser.GameObjects.Container {
 	async slideOut() {
 		await tween({
 			targets: [this],
-			y: -flyoutHeight,
+			y: -SCREEN_HEIGHT,
 		});
 		this.isOpen = false;
 	}
 }
 
-export function addExitButton(flyout: Flyout, onExit: () => void) {
-	const exit = flyout.scene.add.image(
-		0, 0,
-		images.exit.key)
-		.setDisplaySize(200, 200)
-		.setOrigin(0.5)
-		.setInteractive()
-		.setPosition(780, flyout.scene.cameras.main.height - 100)
-		.on("pointerup", async () => {
-			await flyout.slideOut();
-			onExit();
-		});
-
-	const exitText = flyout.scene.add.text(
-		exit.x, exit.y,
-		"Exit",
-		defaultTextConfig,
-	)
-		.setOrigin(0.5)
-		.setFontFamily("Arial Black")
-		.setStroke("black", 14);
-
-	flyout.add([exit, exitText]);
-}
