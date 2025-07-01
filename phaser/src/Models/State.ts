17eacd7614da2d765d0355b7b355ccd2b0abc49b
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-06-30 21:59:26 -0300

    fix: Update import path for vec2 to use Geometry.pure

--- phaser/src/Models/State.ts
+++ phaser/src/Models/State.ts
@@ -1,5 +1,5 @@
 import { Force, playerForce } from "./Entities/Force";
-import { eqVec2, Vec2 } from "./Geometry";
+import { eqVec2, Vec2 } from "./Geometry.pure";
 import { Unit } from "./Entities/Unit";
 
 // Module-scoped variable to hold the state, similar to currentOptions in OptionsStore
