12e18b38096ea45d7a68167fd8b1e423eac0eaa7
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-07-01 11:41:37 -0300

    refactor: Remove obsolete e2e test files and update jest configuration for unit tests

--- phaser/jest.config.js
+++ phaser/jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
 		"^.+\\.(ts|tsx)$": "ts-jest",
 	},
 	moduleFileExtensions: ["ts", "tsx", "js"],
+	testPathIgnorePatterns: [
+		"/node_modules/",
+		"/e2e/",
+		"*.e2e.*",
+	],
 };
