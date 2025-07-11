1f399955c075cdc137259da4e70c34567a3ef13f
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-07-11 14:24:27 -0300

    Remove unused trait effect implementations and their corresponding tests
    - Deleted the following trait effect implementations:
      - positionalBonus.ts
      - reduceEnemyDamage.ts
      - reduceEnemyDamageGlobal.ts
      - reduceEnemyMorale.ts
      - sacrificeCooldownForDamage.ts
      - slowAllEnemies.ts
      - splashDamageToRandomAdjacentAlly.ts
      - stunAllEnemies.ts
      - traitSniper.ts
    
    - Removed associated test files:
      - positionalBonus.test.ts
      - reduceEnemyMorale.test.ts
      - traitSniper.test.ts
      - slowAllEnemies.test.ts
    
    This cleanup helps streamline the codebase by removing redundant or obsolete code.

--- phaser/src/TraitSystem/TraitEffects/implementations/index.ts
+++ phaser/src/TraitSystem/TraitEffects/implementations/index.ts
@@ -3,37 +3,8 @@
  * Exports all individual implementation modules for easy import.
  */
 
-export { dealDamageLogicIO, createDealDamageLogic } from './dealDamage';
-export { addShieldLogicIO, createAddShieldLogic } from './addShield';
-export { grantGoldLogic } from './grantGold';
-export { increaseForceMaxMoraleLogic } from './increaseForceMaxMorale';
-export { modifyStatPassiveLogic } from './modifyStatPassive';
-export { splashDamageToRandomAdjacentAllyLogic } from './splashDamageToRandomAdjacentAlly';
-export { freezeAllEnemiesLogic } from './freezeAllEnemies';
-export { stunAllEnemiesLogic } from './stunAllEnemies';
-export { reduceEnemyDamageGlobalLogic } from './reduceEnemyDamageGlobal';
-export { grantMoraleToAlliesLogic } from './grantMoraleToAllies';
-export { cleanseAllyDebuffsLogic } from './cleanseAllyDebuffs';
-export { chanceToDodgeLogic } from './chanceToDodge';
-export { increasePowerLogic } from './increasePower';
-export { reduceEnemyDamageLogic } from './reduceEnemyDamage';
-export { fortressModePassiveLogic } from './fortressModePassive';
-export { damageScalesWithTimeLogic } from './damageScalesWithTime';
-export { sacrificeCooldownForDamageLogic } from './sacrificeCooldownForDamage';
-export { restoreForceMoraleLogic, restoreForceMoralePure, restoreMoraleLogicIO, createRestoreMoraleLogic } from './restoreMorale';
-export { reduceEnemyMoraleLogic, reduceEnemyMoralePure } from './reduceEnemyMorale';
-export { guildWideDamageLogic, guildWideDamagePure } from './guildWideDamage';
-export { boostAllyDamageLogic, boostAllyDamagePure } from './boostAllyDamage';
-export { hasteAllAlliesLogic, hasteAllAlliesPure } from './hasteAllAllies';
-export { applyPoisonToEnemiesLogic, applyPoisonToEnemiesPure } from './applyPoison';
-export { slowAllEnemies, slowAllEnemiesLogic } from './slowAllEnemies';
-export { traitSniper } from './traitSniper';
-export { performSkillMelee } from './performSkillMelee';
-export { performSkillShoot } from './performSkillShoot';
-export { performSkillHeal } from './performSkillHeal';
-export { performSkillArcaneMissiles } from './performSkillArcaneMissiles';
-export { performSkillHaste } from './performSkillHaste';
-export { performSkillSlow } from './performSkillSlow';
-export { performSkillFireball } from './performSkillFireball';
-export { performSkillSummon } from './performSkillSummon';
-export { positionalBonusLogic } from './positionalBonus';
+export * from './dealDamage';
+export * from './addShield';
+export * from './restoreMorale';
+export * from './modifyStatPassive';
+export * from './grantGold';
