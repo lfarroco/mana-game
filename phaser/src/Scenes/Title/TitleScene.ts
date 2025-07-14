888b9f55061bb37bd1ddceb5345b6370816ec45b
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-07-13 22:41:13 -0300

    refactor: replace direct constant imports with a single constants module import in TitleScene

--- phaser/src/Scenes/Title/TitleScene.ts
+++ phaser/src/Scenes/Title/TitleScene.ts
@@ -1,5 +1,5 @@
 import * as Phaser from "phaser";
-import { SCENE_KEYS, SCREEN_WIDTH, SCREEN_HEIGHT, MIDDLE_SCREEN_X, MIDDLE_SCREEN_Y, titleTextConfig, defaultTextConfig } from "../../constants/constants";
+import * as constants from "../../constants/constants";
 import { State } from "../../Models/State";
 import { UIButton } from "../../UI/UIButton";
 import { CloudsBackground } from "../../components/cloudBackground/CloudsBackground";
@@ -10,7 +10,7 @@ export default class TitleScene extends Phaser.Scene {
 	private cloudsBackground!: CloudsBackground; // Stored for potential future manipulation
 
 	constructor() {
-		super(SCENE_KEYS.TITLE);
+		super(constants.SCENE_KEYS.TITLE);
 	}
 
 	init(data: { state?: State }) {
@@ -32,11 +32,11 @@ export default class TitleScene extends Phaser.Scene {
 
 		// Create the main title
 		this.gameTitle = this.add.text(
-			MIDDLE_SCREEN_X,
-			MIDDLE_SCREEN_Y - 200,
+			constants.MIDDLE_SCREEN_X,
+			constants.MIDDLE_SCREEN_Y - 200,
 			'MANA GAME',
 			{
-				...titleTextConfig,
+				...constants.titleTextConfig,
 				fontSize: '72px',
 				color: '#FFE55C',
 				stroke: '#2D1810',
@@ -46,11 +46,11 @@ export default class TitleScene extends Phaser.Scene {
 
 		// Add a subtitle
 		this.add.text(
-			MIDDLE_SCREEN_X,
-			MIDDLE_SCREEN_Y - 100,
+			constants.MIDDLE_SCREEN_X,
+			constants.MIDDLE_SCREEN_Y - 100,
 			'A Strategic Battle Experience',
 			{
-				...defaultTextConfig,
+				...constants.defaultTextConfig,
 				fontSize: '32px',
 				color: '#E8E8E8',
 				stroke: '#1A1A2E',
@@ -62,8 +62,8 @@ export default class TitleScene extends Phaser.Scene {
 		new UIButton(
 			this,
 			'START GAME',
-			MIDDLE_SCREEN_X,
-			MIDDLE_SCREEN_Y + 100,
+			constants.MIDDLE_SCREEN_X,
+			constants.MIDDLE_SCREEN_Y + 100,
 			() => {
 				this.startGame();
 			}
@@ -80,9 +80,6 @@ export default class TitleScene extends Phaser.Scene {
 			ease: 'Sine.easeInOut'
 		});
 
-		// Add floating particles effect
-		this.createParticles();
-
 		// Allow Enter key to start the game
 		this.input.keyboard?.on('keydown-ENTER', () => {
 			this.startGame();
@@ -91,56 +88,16 @@ export default class TitleScene extends Phaser.Scene {
 		// Note: Preset changing is now handled automatically by the CloudsBackground component
 	}
 
-	private createParticles() {
-		// Create magical floating particles that complement the shader background
-		for (let i = 0; i < 30; i++) {
-			// Vary particle colors to match the mystical theme
-			const colors = [0x6699FF, 0x9966FF, 0xFFAA44, 0x44AAFF, 0xFF6699];
-			const color = colors[Math.floor(Math.random() * colors.length)];
-
-			const particle = this.add.circle(
-				Phaser.Math.Between(0, SCREEN_WIDTH),
-				Phaser.Math.Between(0, SCREEN_HEIGHT),
-				Phaser.Math.Between(1, 4),
-				color,
-				Phaser.Math.Between(0.1, 0.4)
-			);
-
-			// Add a subtle glow effect
-			particle.setBlendMode(Phaser.BlendModes.ADD);
-
-			this.tweens.add({
-				targets: particle,
-				y: particle.y - Phaser.Math.Between(150, 400),
-				x: particle.x + Phaser.Math.Between(-50, 50),
-				alpha: 0,
-				scaleX: Phaser.Math.Between(0.5, 2.0),
-				scaleY: Phaser.Math.Between(0.5, 2.0),
-				duration: Phaser.Math.Between(4000, 8000),
-				delay: Phaser.Math.Between(0, 5000),
-				repeat: -1,
-				ease: 'Sine.easeOut',
-				onRepeat: () => {
-					particle.y = SCREEN_HEIGHT + 10;
-					particle.x = Phaser.Math.Between(0, SCREEN_WIDTH);
-					particle.alpha = Phaser.Math.Between(0.1, 0.4);
-					particle.scaleX = 1;
-					particle.scaleY = 1;
-				}
-			});
-		}
-	}
-
 	private startGame() {
 		// Transition to the battleground scene
 		this.cameras.main.fade(500, 0, 0, 0);
 		this.cameras.main.once('camerafadeoutcomplete', () => {
 			if (this.state) {
-				this.scene.start(SCENE_KEYS.BATTLEGROUND, this.state);
+				this.scene.start(constants.SCENE_KEYS.BATTLEGROUND, this.state);
 			} else {
 				// If no state is provided, we might need to create a default one
 				// or handle this case according to your game's logic
-				this.scene.start(SCENE_KEYS.BATTLEGROUND);
+				this.scene.start(constants.SCENE_KEYS.BATTLEGROUND);
 			}
 		});
 	}
