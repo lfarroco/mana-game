4bcd266ffd35e6b761a49a43e1e448f706b5b674
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-07-26 20:06:12 -0300

    refactor: simplify card registration by removing slice in collection loading

--- phaser/src/Models/Entities/Card.ts
+++ phaser/src/Models/Entities/Card.ts
@@ -19,7 +19,7 @@ export const registerCollection = (collection: CardCollection): void => {
   }
   collections.set(collection.id, collection);
 
-  collection.cards.slice(45, 50).forEach(registerCard);
+  collection.cards.forEach(registerCard);
 
 };
 
