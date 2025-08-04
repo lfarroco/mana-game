38447ba74f9e9e692f115b44a9c7f95dd8233acf
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-08-03 21:10:24 -0300

    add build commands for multiple platforms

--- phaser/electron.js
+++ phaser/electron.js
@@ -1,10 +1,36 @@
 const { app, BrowserWindow } = require('electron');
-
-// continue from here https://www.electronjs.org/docs/latest/tutorial/tutorial-first-app
+const path = require('path');
 
 function createWindow() {
-	const mainWindow = new BrowserWindow(/* your options */);
-	mainWindow.loadFile('dist/index.html');
+	const mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		webPreferences: {
+			nodeIntegration: false,
+			contextIsolation: true
+		},
+		icon: path.join(__dirname, 'assets/icon.png') // You can add an icon later
+	});
+
+	// Load the app
+	if (process.env.NODE_ENV === 'development') {
+		mainWindow.loadURL('http://localhost:8080');
+		mainWindow.webContents.openDevTools();
+	} else {
+		mainWindow.loadFile(path.join(__dirname, 'dist/index.html'));
+	}
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
