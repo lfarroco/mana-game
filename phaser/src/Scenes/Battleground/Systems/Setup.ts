e353968eabacab4dbc9d5f4621e9cbe2599c990e
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-08-24 23:49:23 -0300

    refactor: remove BattlegroundSetupSystem and integrate its functionality into Setup module

---
+++ phaser/src/Scenes/Battleground/Systems/Setup.ts
@@ -0,0 +1,46 @@
+import { State } from "../../../Models/State";
+import * as ControlsSystem from "../../../Systems/Controls/Controls";
+import { initializePlayerBoard, PartyBoard, createBoardDropZone } from "../../../Models/Board";
+import * as BG_CONSTANTS from "../battlegroundConstants";
+import { scene } from "../BattlegroundScene";
+import { getOption } from "../../../Models/OptionsStore";
+import { CloudsBackground } from "../../../components/cloudBackground/CloudsBackground";
+import * as UIManager from "../../../UI/UIManager";
+
+export let cloudsBackground: CloudsBackground | null = null;
+
+
+export function initializeNewGame(state: State): void {
+
+	state.gameData.player.units = [];
+	state.gameData.round = 1;
+	state.gameData.player.prestige = 0;
+	state.gameData.player.gold = BG_CONSTANTS.INITIAL_PLAYER_GOLD;;
+	UIManager.updatePrestige(state.gameData.player.prestige, 0);
+
+	scene.sound.setVolume(getOption('soundVolume') ?? BG_CONSTANTS.DEFAULT_SCENE_SOUND_VOLUME);
+}
+
+export function setupSceneElements(_state: State): PartyBoard {
+	cloudsBackground = new CloudsBackground(scene, {
+		preset: 'forest',
+		depth: -2000,
+		timeScale: 0.3
+	});
+
+	scene.cloudsBackground = cloudsBackground.getShader() as any;
+
+	scene.bgContainer = scene.add.container(0, 0);
+	ControlsSystem.init(scene);
+
+	scene.bgContainer.add([scene.cloudsBackground]);
+
+	const playerBoard = initializePlayerBoard(scene);
+	createBoardDropZone();
+	return playerBoard;
+}
+
+export function destroy(): void {
+	cloudsBackground?.destroy();
+	cloudsBackground = null;
+}
