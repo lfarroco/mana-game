3356878b7e6dc1be8012aeb57b2f70418bfa27c3
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-08-25 00:15:39 -0300

    refactor: reorganize Chara input handling and animations, consolidate functionality into dedicated modules

---
+++ phaser/src/Systems/Chara/input/onDragStart.ts
@@ -0,0 +1,37 @@
+import Phaser from "phaser";
+import { vec2 } from "../../../Models/Geometry.pure";
+import { scene } from "../../../Scenes/Battleground/BattlegroundScene";
+import * as Shop from "../../../Scenes/Battleground/Systems/Shop";
+import { hideTooltip } from "../../../UI/Tooltip";
+import { tween } from "../../../Utils/animation";
+import { getIsShopItem } from "../Chara";
+import { CharaInputHandler } from "./CharaInputHandler";
+
+export const onDragStart = (handlerState: CharaInputHandler) => (
+	_pointer: Phaser.Input.Pointer,
+	_dragX: number,
+	_dragY: number
+) => {
+	const { chara } = handlerState;
+	handlerState.dragStartX = chara.x;
+	handlerState.dragStartY = chara.y;
+	handlerState.dragStartVec = vec2(handlerState.dragStartX, handlerState.dragStartY);
+	handlerState.wasDragSuccessful = false;
+
+	if (getIsShopItem(handlerState.unitId)) {
+		Shop.UI.bringShopChildToTop(chara);
+	} else {
+		scene.children.bringToTop(chara);
+	}
+
+	tween({
+		targets: [chara],
+		angle: -10,
+		duration: 100,
+		ease: "Cubic.Out",
+	});
+	if (!getIsShopItem(handlerState.unitId)) {
+		Shop.UI.showSellZone();
+	}
+	hideTooltip();
+};
