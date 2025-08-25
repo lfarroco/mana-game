1cb9230e12c358efcff94ca4bd38791bf4a324e6
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-08-24 23:30:21 -0300

    refactor: rename ShopConstants to constants and update imports

--- phaser/src/Scenes/Battleground/Systems/Shop/index.ts
+++ phaser/src/Scenes/Battleground/Systems/Shop/index.ts
@@ -1,4 +1,4 @@
 export * as Shop from "./Shop";
-export * as constants from "./ShopConstants"
+export * as constants from "./constants"
 export * as UI from "./ShopUI";
 export * as events from "./events";
