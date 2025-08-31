8fd6d33f0a41244a024e25c7033c03d107c868cb
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-08-31 20:06:21 -0300

    add results panel

--- phaser/src/Scenes/Battleground/Results/VictoryUI.ts
+++ phaser/src/Scenes/Battleground/Results/VictoryUI.ts
@@ -2,6 +2,7 @@ import { scene } from "../BattlegroundScene";
 import { createUIButton } from "../../../UI/UIButton";
 import * as c from "../../../constants/constants";
 import { slideOut, ResultsUIState } from "./ResultsUI";
+import { createResultsPanel } from "./Panel";
 
 export function displayVictory(
 	state: ResultsUIState,
@@ -9,33 +10,7 @@ export function displayVictory(
 	prestigeChange: number,
 	nextPhaseCallback: () => void
 ): void {
-	// Create background overlay to block interactions
-	if (state.backgroundOverlay) {
-		state.backgroundOverlay.destroy();
-	}
-	state.backgroundOverlay = scene.add.rectangle(
-		c.SCREEN_WIDTH / 2,
-		c.SCREEN_HEIGHT / 2,
-		c.SCREEN_WIDTH,
-		c.SCREEN_HEIGHT,
-		0x000000,
-		0
-	);
-	state.backgroundOverlay.setInteractive();
-	state.backgroundOverlay.setDepth(1000);
-
-	const screenWidth = scene.cameras.main.width;
-	const panelX = screenWidth - 600 - 40;
-	const panelY = 240;
-	const panelWidth = 600;
-	const panelHeight = 500;
-
-	// Create background panel
-	const resultsBackground = scene.add.graphics()
-		.fillStyle(0x2c3e50, 0.95)
-		.fillRoundedRect(panelX, panelY, panelWidth, panelHeight, 20);
-	resultsBackground.setDepth(1001);
-	state.resultsContainer.add(resultsBackground);
+	const { panelX, panelY, panelWidth, panelHeight } = createResultsPanel(state);
 
 	// Add title
 	const titleText = "Victory!";
