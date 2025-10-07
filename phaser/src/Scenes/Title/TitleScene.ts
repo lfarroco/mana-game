5d2d45fc8fd09265c586b2df14ace9a9a2342511
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-10-07 16:08:56 -0300

    Revert "rename lib"
    This reverts commit 5466be049870e792b45b8c0a1d62c5184d4f37c5.

--- phaser/src/Scenes/Title/TitleScene.ts
+++ phaser/src/Scenes/Title/TitleScene.ts
@@ -4,7 +4,7 @@ import { createUIButton } from "../../UI/UIButton";
 import { CloudsBackground } from "../../components/cloudBackground/CloudsBackground";
 import { images } from "../../assets";
 import * as AudioManager from "@Systems/AudioManager";
-import { createReactiveDemo } from "../../mana";
+import { createReactiveDemo } from "../../ph-ts";
 
 export let titleScene: TitleScene;
 
