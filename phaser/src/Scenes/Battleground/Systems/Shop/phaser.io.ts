ce513ada809e553962968525a6388b2458028a6f
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-10-11 13:30:19 -0300

    adding phaser.io

---
+++ phaser/src/Scenes/Battleground/Systems/Shop/phaser.io.ts
@@ -0,0 +1,80 @@
+import { getState } from "@Models/State";
+
+export function BringToTop(obj: Phaser.GameObjects.GameObject) {
+	const scene = getState().currentScene;
+	scene.children.bringToTop(obj);
+}
+
+export function Container(
+) {
+	const scene = getState().currentScene;
+	return scene.add.container();
+}
+
+export function AddChildren(
+	container: Phaser.GameObjects.Container,
+	children: Phaser.GameObjects.GameObject[]
+) {
+	container.add(children)
+}
+
+export function SetPosition(obj: { setPosition: (x: number, y: number) => void }, vec: Vec2) {
+	obj.setPosition(vec.x, vec.y);
+}
+
+export function SetVisible(obj: { setVisible: (visible: boolean) => void }, visible: boolean) {
+	obj.setVisible(visible);
+}
+
+export function Destroy(obj: Phaser.GameObjects.GameObject) {
+	obj.destroy(true);
+}
+export function BorderedRoundRect(
+	position: Vec2,
+	size: Dimension,
+	cornerRadius: number = 10,
+	color: number = 0xffa500,
+	alpha: number = 0.7) {
+	const scene = getState().currentScene;
+	const g = scene.add.graphics(position);
+	g.lineStyle(4, 0xffffff, 0.8);
+	g.fillStyle(color, alpha);
+	g.fillRoundedRect(0, 0, size.width, size.height, cornerRadius);
+	g.strokeRoundedRect(0, 0, size.width, size.height, cornerRadius);
+
+	return g;
+}
+
+export function RectangularDropZone(
+	name: string,
+	{ x, y }: Vec2,
+	{ width, height }: Dimension,
+) {
+	const scene = getState().currentScene;
+
+	const zone = scene.add.zone(
+		x, y,
+		width, height
+	);
+
+	zone.setName(name);
+
+	zone.setRectangleDropZone(width, height); //redundant??
+
+	return zone;
+}
+
+export function Centralize(obj: {
+	setOrigin: (n: number) => void
+}) {
+	obj.setOrigin(0.5);
+}
+
+export function Text(
+	position: Vec2,
+	text: string,
+	style: Phaser.Types.GameObjects.Text.TextStyle
+) {
+	const scene = getState().currentScene;
+	return scene.add.text(position.x, position.y, text, style);
+}
