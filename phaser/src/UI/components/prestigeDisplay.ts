db55054443ce806041561c840372886f504930d5
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-10-25 18:32:39 -0300

    refactors

--- phaser/src/UI/components/prestigeDisplay.ts
+++ phaser/src/UI/components/prestigeDisplay.ts
@@ -3,17 +3,42 @@ import { vec2 } from "@Models/Geometry";
 import { getState } from "@Models/State";
 import * as io from "@PhaserIO";
 
-export let prestigeTextElement: TextObj | null = null;
+export let textEl: TextObj | null = null;
 export const updatePrestigeDisplay = (newTotalPrestige: number): void => {
-	prestigeTextElement!.setText(newTotalPrestige.toString());
+	textEl!.setText(newTotalPrestige.toString());
 };
 export const PRESTIGE_DISPLAY_X = c.SCREEN_WIDTH - 520;
 export const PRESTIGE_DISPLAY_Y = 20;
 
-export function createPrestigeDisplay() {
+export function create() {
 
 	const initialPrestige = getState().gameData.player.prestige;
 
+	const label_ = label();
+
+	const text_ = text(initialPrestige, label_);
+
+	const container = io.Container([label_, text_])
+	io.SetPosition(container, vec2(PRESTIGE_DISPLAY_X, PRESTIGE_DISPLAY_Y));
+
+	return container;
+}
+
+function text(initialPrestige: number, label: Phaser.GameObjects.Text) {
+	textEl = io.Text(
+		initialPrestige.toString(),
+		{
+			...c.titleTextConfig,
+			fontSize: '24px',
+			color: '#ffffff'
+		}
+	);
+	io.SetPosition(textEl, vec2(label.width + 10, 0));
+	io.Centralize(textEl);
+	return textEl;
+}
+
+function label() {
 	const label = io.Text(
 		"Prestige:",
 		{
@@ -23,22 +48,6 @@ export function createPrestigeDisplay() {
 		}
 	);
 	io.Centralize(label);
-
-	prestigeTextElement = io.Text(
-		initialPrestige.toString(),
-		{
-			...c.titleTextConfig,
-			fontSize: '24px',
-			color: '#ffffff'
-		}
-	);
-	io.SetPosition(prestigeTextElement, vec2(label.width + 10, 0));
-	io.Centralize(prestigeTextElement);
-
-	const prestigeContainer = io.Container();
-	io.SetPosition(prestigeContainer, vec2(PRESTIGE_DISPLAY_X, PRESTIGE_DISPLAY_Y));
-
-	io.AddChildren(prestigeContainer, [label, prestigeTextElement]);
-
-	return prestigeContainer;
+	return label;
 }
+
