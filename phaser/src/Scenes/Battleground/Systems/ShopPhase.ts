f170d18d6e231ca8f22811860147bc68073d440a
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-10-25 22:49:52 -0300

    refactor phases wip

--- phaser/src/Scenes/Battleground/Systems/ShopPhase.ts
+++ phaser/src/Scenes/Battleground/Systems/ShopPhase.ts
@@ -1,67 +1,12 @@
 import { getState } from "@Models/State";
-import { delay } from "@Utils/animation";
-import { cpuForce } from "@Models/Entities/Force";
 import * as PrestigeSystem from "@Systems/PrestigeSystem";
 import { renderVignette } from "../Animations/vignette";
-import { clearAll, summon } from "@Systems/Chara/Chara";
-import * as HeroShop from "./Shop/HeroShop";
 import * as OrbShop from "./Shop/OrbShop";
-import * as MoraleDisplay from "../MoraleDisplay";
-import { clearRegen } from "./RegenSystem";
-import { clearPoison } from "./PoisonDamageSystem";
-import * as c from "@Constants/constants";
-import * as BoardStatsDisplay from "../BoardStatsDisplay";
+import { resetBoard } from "../PhaseManager";
 
-async function setupShopPhaseCommon(shouldResummonUnits: boolean = true): Promise<void> {
-	const state = getState();
-	if (shouldResummonUnits) {
-		clearAll();
-		state.battleData.units = [];
-	}
-
-	const playerForce = state.gameData.player;
-	playerForce.morale = playerForce.maxMorale;
-	playerForce.shield = 0;
-	MoraleDisplay.updateMoraleBar(c.FORCE_ID_PLAYER);
-	MoraleDisplay.updateShieldBar(c.FORCE_ID_PLAYER, 0, playerForce.maxMorale);
-
-	cpuForce.morale = cpuForce.maxMorale;
-	cpuForce.shield = 0;
-	MoraleDisplay.updateMoraleBar(c.FORCE_ID_CPU);
-	MoraleDisplay.updateShieldBar(c.FORCE_ID_CPU, 0, cpuForce.maxMorale);
-
-	clearRegen(c.FORCE_ID_PLAYER);
-	clearRegen(c.FORCE_ID_CPU);
-	clearPoison(c.FORCE_ID_PLAYER);
-	clearPoison(c.FORCE_ID_CPU);
-
-	BoardStatsDisplay.updateStats(c.FORCE_ID_PLAYER);
-	BoardStatsDisplay.updateStats(c.FORCE_ID_CPU);
-
-	if (shouldResummonUnits) {
-		const summonPromises = state.gameData.player.units
-			.map(async (unit, index) => {
-				await delay(index * 200);
-				await summon(unit, true);
-			});
-		await Promise.all(summonPromises);
-	}
-
-	BoardStatsDisplay.hideCpuStats();
-}
-
-
-export async function initializeShopPhase(): Promise<void> {
-	await setupShopPhaseCommon(true);
-
-	const state = getState();
-	console.log("Round", state.gameData.round, "Shop Phase Starting (Initial Setup).");
-
-	//HeroShop.handleShopOpenUITrigger();
-}
-
+//TODO: motve to combat phase
 export async function transitionToNextPhaseAfterVictory(): Promise<void> {
-	await setupShopPhaseCommon(true);
+	await resetBoard(true);
 
 	PrestigeSystem.processVictory();
 	PrestigeSystem.finalizeRound();
@@ -73,8 +18,9 @@ export async function transitionToNextPhaseAfterVictory(): Promise<void> {
 	//HeroShop.handleShopOpenUITrigger();
 }
 
+//TODO: motve to combat phase
 export async function transitionToNextPhaseAfterDefeat(): Promise<void> {
-	await setupShopPhaseCommon(true);
+	await resetBoard(true);
 
 	PrestigeSystem.processDefeat();
 	PrestigeSystem.finalizeRound();
@@ -91,16 +37,9 @@ export async function transitionToNextPhaseAfterDefeat(): Promise<void> {
 	//HeroShop.handleShopOpenUITrigger();
 }
 
-export async function transitionToNextShopPhase(): Promise<void> {
-	await setupShopPhaseCommon(false);
-
-	console.log("Round", getState(), "Shop Phase", "Starting.");
-
-	HeroShop.open();
-}
 
 export async function transitionToOrbShopPhase(): Promise<void> {
-	await setupShopPhaseCommon(false);
+	await resetBoard(false);
 
 	const state = getState();
 	console.log("Round", state.gameData.round, "Orb Shop Phase Starting.");
