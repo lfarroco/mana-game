2fb30e0d9f925c09cf5e8a589066bea5845bf66b
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-10-28 20:13:46 -0300

    title button changes

--- phaser/src/Game/effects/loadGame.ts
+++ phaser/src/Game/effects/loadGame.ts
@@ -1,10 +1,10 @@
 import { SCENE_KEYS } from "@Constants/constants";
 import { getState, getCurrentScene } from "@Models/State";
+import { getSavedData } from "./getSavedData";
 
 export function loadGame() {
-	const data = localStorage.getItem("gameData");
-	if (data) {
-		getState().gameData = JSON.parse(data);
-		getCurrentScene().scene.start(SCENE_KEYS.BATTLEGROUND, JSON.parse(data));
-	}
+	const data = getSavedData();
+	if (!data) return;
+	getState().gameData = JSON.parse(data);
+	getCurrentScene().scene.start(SCENE_KEYS.BATTLEGROUND, JSON.parse(data));
 }
