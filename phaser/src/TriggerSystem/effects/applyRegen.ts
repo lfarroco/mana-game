af037ff1514c5170fc4ba05b9bb67d1e070d0aac
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-11-06 20:00:14 -0300

    regen crit

--- phaser/src/TriggerSystem/effects/applyRegen.ts
+++ phaser/src/TriggerSystem/effects/applyRegen.ts
@@ -1,5 +1,5 @@
 import { getAlliedCore } from '@Models/Entities/Card';
-import { Unit } from '@Models/Entities/Unit';
+import { isCritical, Unit } from '@Models/Entities/Unit';
 import { getState } from '@Models/State';
 import { applyRegen } from '@Scenes//Battleground/Systems/RegenSystem';
 import { getCharaById } from '@Systems/Chara/Chara';
@@ -7,15 +7,18 @@ import { arcaneMissileTargeted } from '../../Effects';
 
 export const applyRegenLogicIO = async (sourceUnit: Unit) => {
 
-	const amount = sourceUnit.power * 0.1;
+	const baseAmount = sourceUnit.power * 0.1;
+
+	const critical = isCritical(sourceUnit);
+
+	const amount = critical ? baseAmount * 2 : baseAmount;
 
 	const targetForce = getState().battleData.forces.find(force => force.id === sourceUnit.force)!;
 
 	console.log(`[ApplyRegen] Unit power: ${sourceUnit.power}, Regen rate: ${amount}, Total healing over time: ${amount * 10}`);
 
-	const effect = () => {
-		applyRegen(targetForce, amount, sourceUnit.id);
-	}
+	const effect = () =>
+		applyRegen(targetForce, amount, sourceUnit.id, critical);
 
 	const alliedCore = getAlliedCore(sourceUnit.force);
 
