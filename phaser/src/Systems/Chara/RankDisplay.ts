56f87444cfbbd22d0a1e530481afbc9af846f439
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-11-12 12:24:23 -0300

    rank colors refactor

--- phaser/src/Systems/Chara/RankDisplay.ts
+++ phaser/src/Systems/Chara/RankDisplay.ts
@@ -4,18 +4,25 @@ import { MagicOrb } from '@Components/MagicOrb/MagicOrb';
 import { hexToVector3 } from '@Utils/colorUtils';
 import { TILE_WIDTH } from '@Constants/constants';
 
+const bronze = 0x804a00;
+const silver = 0xC0C0C0;
+const gold = 0xFFD700;
+const platinum = 0xB9F2FF;
+
+const colors = [
+	bronze,
+	silver,
+	gold,
+	platinum
+];
 
 export function create(unit: Unit, chara: Chara) {
 
+	const color = colors[unit.rank - 1] || bronze;
+
 	const orb = new MagicOrb(0, 0, {
 		size: TILE_WIDTH * 0.7,
-		color: hexToVector3(
-
-			unit.rank === 1 ? 0xCD7F32 :
-				unit.rank === 2 ? 0xC0C0C0 :
-					unit.rank === 3 ? 0xFFD700 : 0xB9F2FF
-
-		),
+		color: hexToVector3(color),
 		intensity: 1.2,
 		speed: 1.0,
 	});
