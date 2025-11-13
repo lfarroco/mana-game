67afc9aba862e07511ec2e27ea226cfe6a53b751
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-11-13 14:59:36 -0300

    adjust chip size

--- phaser/src/components/Chip.ts
+++ phaser/src/components/Chip.ts
@@ -1,10 +1,11 @@
 import { defaultTextConfig } from "@Constants/constants";
-import { asVec2, size } from "@Models/Geometry";
+import { asVec2, size, sumVec2 } from "@Models/Geometry";
 import * as io from "@PhaserIO";
 
 const index = new Map<string, {
 	bg: Graphics;
 	text: Phaser.GameObjects.Text;
+	color: number;
 }>();
 
 
@@ -25,8 +26,8 @@ export function createChip(
 	const bg = io.BorderedRoundRect(
 		asVec2(text),
 		size(
-			text.width + 4,
-			text.height + 4,
+			text.width + 12,
+			text.height + 12,
 		),
 		4,
 		color
@@ -34,7 +35,7 @@ export function createChip(
 
 	io.MoveBelow(bg, text)
 
-	index.set(id, { bg, text, })
+	index.set(id, { bg, text, color })
 
 	io.OnceDestroyed(bg, () => {
 		index.delete(id);
@@ -48,4 +49,16 @@ export function updateChipText(id: string, value: string) {
 	if (!state) return;
 
 	state.text.setText(value);
+
+	const { bg, text, color } = state;
+	const newSize = { width: text.width + 12, height: text.height + 12 };
+	const textPosition = asVec2(text);
+	const newActualPos = sumVec2(textPosition, { x: -newSize.width / 2, y: -newSize.height / 2 });
+
+	bg.clear();
+	bg.lineStyle(2, 0xffffff, 0.5);
+	bg.fillStyle(color, 0.7);
+	bg.fillRoundedRect(0, 0, newSize.width, newSize.height, 4);
+	bg.strokeRoundedRect(0, 0, newSize.width, newSize.height, 4);
+	bg.setPosition(newActualPos.x, newActualPos.y);
 }
