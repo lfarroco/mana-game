8eea289d7980a9f6daaec4682c0d3a990512b036
Leonardo Farroco <mana-battle@users.noreply.github.com>
2025-11-19 10:29:50 -0300

    balance changes

--- phaser/src/TriggerSystem/TriggerSystem.ts
+++ phaser/src/TriggerSystem/TriggerSystem.ts
@@ -196,43 +196,43 @@ function processReactions(triggeringUnit: Unit, effect: Effect) {
 		.battleData.units.filter((u) => u.force === triggeringUnit.force)
 		.filter((u) => u.id != triggeringUnit.id)
 		.forEach((u) => {
-			const reactions = u.reactions.filter((r) => {
-				if (r.effectId !== effect.id && r.effectId !== "all") {
-					return false;
-				}
-
-				switch (r.position) {
-					case "all":
-						return true;
-					case "allies":
-						return u.force === triggeringUnit.force;
-					case "enemies":
-						return u.force !== triggeringUnit.force;
-					case "row_allies":
-						return u.force === triggeringUnit.force && u.position.x === u.position.x;
-					case "column_allies":
-						return u.force === triggeringUnit.force && u.position.y === u.position.y;
-					case "top_ally":
-						return (
-							u.force === triggeringUnit.force && triggeringUnit?.position.y - 1 === u.position.y
-						);
-					case "bottom_ally":
-						return (
-							u.force === triggeringUnit.force && triggeringUnit?.position.y + 1 === u.position.y
-						);
-					case "left_ally":
-						return (
-							u.force === triggeringUnit.force && triggeringUnit?.position.x - 1 === u.position.x
-						);
-					case "right_ally":
-						return (
-							u.force === triggeringUnit.force && triggeringUnit?.position.x + 1 === u.position.x
-						);
-					default:
-						const _exhaustiveCheck: never = r.position;
-						return _exhaustiveCheck;
-				}
-			});
+			const reactions = u.reactions
+				.filter(r =>
+					r.effectId === effect.id || r.effectId === "all"
+				)
+				.filter((r) => {
+					switch (r.position) {
+						case "all":
+							return true;
+						case "allies":
+							return u.force === triggeringUnit.force;
+						case "enemies":
+							return u.force !== triggeringUnit.force;
+						case "row_allies":
+							return u.force === triggeringUnit.force && u.position.x === u.position.x;
+						case "column_allies":
+							return u.force === triggeringUnit.force && u.position.y === u.position.y;
+						case "top_ally":
+							return (
+								u.force === triggeringUnit.force && triggeringUnit?.position.y - 1 === u.position.y
+							);
+						case "bottom_ally":
+							return (
+								u.force === triggeringUnit.force && triggeringUnit?.position.y + 1 === u.position.y
+							);
+						case "left_ally":
+							return (
+								u.force === triggeringUnit.force && triggeringUnit?.position.x - 1 === u.position.x
+							);
+						case "right_ally":
+							return (
+								u.force === triggeringUnit.force && triggeringUnit?.position.x + 1 === u.position.x
+							);
+						default:
+							const _exhaustiveCheck: never = r.position;
+							return _exhaustiveCheck;
+					}
+				});
 
 			reactions.forEach((r) => {
 				processEffectsIO(u, r.effects);
