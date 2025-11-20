7dff48671dcaa9614c03d2b5f20d8a99396e6cfd
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-11-20 09:50:49 -0300

    ui background

---
+++ phaser/src/UI/components/headerBackground.ts
@@ -0,0 +1,27 @@
+import { scene } from "@Scenes/Battleground/BattlegroundScene";
+
+const COLOR_DARK_GRAY = 0x1f1f1f;
+const ALPHA = 0.8;
+
+const TOP_Y = 0;
+const HEIGHT = 70;
+const START_X = 280;
+const END_X = 920;
+const SLOPE = 40;
+
+export function create() {
+	const graphics = scene.add.graphics();
+
+	graphics.fillStyle(COLOR_DARK_GRAY, ALPHA);
+
+	const points = [
+		{ x: START_X, y: TOP_Y },
+		{ x: END_X, y: TOP_Y },
+		{ x: END_X - SLOPE, y: TOP_Y + HEIGHT },
+		{ x: START_X + SLOPE, y: TOP_Y + HEIGHT }
+	];
+
+	graphics.fillPoints(points, true);
+
+	return graphics;
+}
