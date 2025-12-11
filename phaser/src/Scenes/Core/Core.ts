2db3203a559fdf7c748d7766d548b2632545eb1d
Leonardo Farroco <leonardo.farroco@gmail.com>
2025-12-10 23:50:59 -0300

    add gold/silver shop

--- phaser/src/Scenes/Core/Core.ts
+++ phaser/src/Scenes/Core/Core.ts
@@ -37,7 +37,9 @@ export default class Core extends Phaser.Scene {
 		this.load.image("ui/toxic", "assets/ui/toxic.png");
 		this.load.image("ui/trial_circuit", "assets/ui/trial_circuit.png");
 		this.load.image("ui/upgrade_unit", "assets/ui/upgrade_unit.png");
-		this.load.image("ui/youtube_icon", "assets/ui/youtube_icon.png");
+
+		this.load.image("ui/silver_medal", "assets/ui/silver_medal.png");
+		this.load.image("ui/gold_medal", "assets/ui/gold_medal.png");
 
 		//sfx_ui_error.m4a
 		this.load.audio("sfx_ui_error", "assets/audio/sfx_ui_error.m4a");
