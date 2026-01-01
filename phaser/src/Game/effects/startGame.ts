de9861e3278dc08678142dbfb9281a25e8e886b2
Leonardo Farroco <mana-battle@users.noreply.github.com>
2026-01-01 20:23:03 -0300

    feat: allow resuming active multiplayer arena sessions by persisting player IDs and checking server state

--- phaser/src/Game/effects/startGame.ts
+++ phaser/src/Game/effects/startGame.ts
@@ -1,7 +1,18 @@
 import * as constants from "@Constants/constants";
 import * as io from "@PhaserIO";
+import { MultiplayerManager } from "../../Multiplayer/MultiplayerManager";
 
 export async function startGame(options?: any) {
 	await io.Fade(300, 0x000000);
+
+	if (options?.isArena) {
+		const hasActive = await MultiplayerManager.getInstance().checkActiveSession();
+		if (hasActive) {
+			await MultiplayerManager.getInstance().enableMultiplayer();
+			io.StartScene(constants.SCENE_KEYS.BATTLEGROUND, options);
+			return;
+		}
+	}
+
 	io.StartScene(constants.SCENE_KEYS.CRYSTAL_SELECTION, options);
 }
