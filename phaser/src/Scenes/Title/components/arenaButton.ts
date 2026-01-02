a974e9943e2eba23b3020f27606c9d5310fe9c8b
Leonardo Farroco <leonardo.farroco@gmail.com>
2026-01-02 20:30:25 -0300

    feat: internationalize Arena button text and add corresponding English translations

--- phaser/src/Scenes/Title/components/arenaButton.ts
+++ phaser/src/Scenes/Title/components/arenaButton.ts
@@ -3,18 +3,20 @@ import { vec2 } from "@Models/Geometry";
 import { createUIButton } from "@Components/UIButton";
 import { startGame } from "../../../Game/effects/startGame";
 import { MultiplayerManager } from "../../../Multiplayer/MultiplayerManager";
+import { t } from "@i18n/i18n";
 
 
 export function arenaButton(y: number) {
 	const btn = createUIButton(
-		"Arena", // TODO: Add translation t("title.arena")
+		t("title.arena"),
 		vec2(constants.MIDDLE_SCREEN.x, y),
 		() => startGame({ isArena: true })
 	);
 
 	MultiplayerManager.getInstance().checkActiveSession().then((isActive) => {
 		if (isActive) {
-			btn.text.setText("Arena (Continue)");
+			btn.text.setText(
+				t("title.arena_continue"));
 		}
 	});
 
