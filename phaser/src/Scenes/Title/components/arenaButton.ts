9778689846e26378583be435459377c3fb2b72a7
Leonardo Farroco <leonardo.farroco@gmail.com>
2026-01-02 20:13:05 -0300

    feat: conditionally update Arena button text based on active multiplayer session

--- phaser/src/Scenes/Title/components/arenaButton.ts
+++ phaser/src/Scenes/Title/components/arenaButton.ts
@@ -2,12 +2,21 @@ import * as constants from "@Constants/constants";
 import { vec2 } from "@Models/Geometry";
 import { createUIButton } from "@Components/UIButton";
 import { startGame } from "../../../Game/effects/startGame";
+import { MultiplayerManager } from "../../../Multiplayer/MultiplayerManager";
 
 
 export function arenaButton(y: number) {
-	return createUIButton(
+	const btn = createUIButton(
 		"Arena", // TODO: Add translation t("title.arena")
 		vec2(constants.MIDDLE_SCREEN.x, y),
 		() => startGame({ isArena: true })
 	);
+
+	MultiplayerManager.getInstance().checkActiveSession().then((isActive) => {
+		if (isActive) {
+			btn.text.setText("Arena (Continue)");
+		}
+	});
+
+	return btn;
 }
