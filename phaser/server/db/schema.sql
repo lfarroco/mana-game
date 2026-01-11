355f33587baeea72c6b24f80321e7d9a316ab067
Leonardo Farroco <mana-battle@users.noreply.github.com>
2026-01-10 21:06:36 -0300

    fix table schema

--- phaser/server/db/schema.sql
+++ phaser/server/db/schema.sql
@@ -24,6 +24,7 @@ create table public.player_sessions (
   current_options jsonb,
   wins integer not null default 0,
   losses integer not null default 0,
+  team jsonb,
   created_at timestamp with time zone not null default now(),
   updated_at timestamp with time zone not null default now(),
   constraint player_sessions_pkey primary key (id),
