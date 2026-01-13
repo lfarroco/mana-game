e18f9a7f062d6999571910b883ffbdaaae8f3991
Leonardo Farroco <leonardo.farroco@gmail.com>
2026-01-13 12:54:32 -0300

    fix electron build

--- phaser/electron/main.cjs
+++ phaser/electron/main.cjs
@@ -31,7 +31,7 @@ function createWindow() {
 			// Required for steamworks.js to work
 			nodeIntegration: true,
 			contextIsolation: false,
-			preload: path.join(__dirname, 'preload.js')
+			preload: path.join(__dirname, 'preload.cjs')
 		},
 		icon: path.join(__dirname, 'icon.png')
 	});
