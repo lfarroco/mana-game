060e5dd6e672ab9fa78d288a20779cbdddad2089
Leonardo Farroco <leonardo.farroco@gmail.com>
2026-02-02 10:32:14 -0300

    remove legacy code

--- phaser/src/Scenes/Battleground/Systems/Shop/HeroShop.ts
+++ phaser/src/Scenes/Battleground/Systems/Shop/HeroShop.ts
@@ -1,5 +1,4 @@
 import * as Card from "@Models/Entities/Card";
-import { pickRandom } from "../../../../utils";
 import * as Chara from "@Systems/Chara/Chara";
 import * as ShopPanel from "./ShopPanel";
 import * as CharaShop from "./CharaShop";
@@ -7,7 +6,6 @@ import * as sc from "./constants";
 import { tween } from "@Utils/animation";
 import * as Board from "@Models/Board";
 import { getCurrentScene, getState } from "@Models/State";
-import * as StatsStore from "@Models/StatsStore";
 import { MultiplayerManager } from "../../../../Multiplayer/MultiplayerManager";
 import { getServerAdapter } from "@Core/ServerFactory";
 import { handlePhaseEnded } from "@Scenes/Battleground/PhaseManager";
@@ -17,8 +15,8 @@ import * as PhaseManager from "@Scenes/Battleground/PhaseManager";
 let currentShopCharas: Chara.Chara[] = [];
 
 export async function openHeroShop(
-	filter?: (u: Card.CardDefinition) => boolean,
-	totalHeroes?: number,
+	_filter?: (u: Card.CardDefinition) => boolean,
+	_totalHeroes?: number,
 	serverCardIds?: string[]
 ): Promise<void> {
 	return new Promise<void>(async (resolve) => {
@@ -29,7 +27,8 @@ export async function openHeroShop(
 		if (serverCardIds) {
 			tavernCardData = serverCardIds.map(id => Card.getCardDefinition(id));
 		} else {
-			tavernCardData = getAvailableCardsForTavern(totalHeroes || sc.NUM_TAVERN_SLOTS, filter);
+			console.warn("No server card IDs provided to hero shop");
+			tavernCardData = [];
 		}
 
 		const finishPhaseCallback = async () => {
@@ -111,19 +110,4 @@ async function animateItemAppearance(chara: Chara.Chara) {
 			{ angle: 0, duration: sc.SHOP_ITEM_APPEAR_WIGGLE_RETURN_DURATION, ease: "Quad.easeIn" },
 		],
 	});
-}
-
-function getAvailableCardsForTavern(count: number, filter?: (u: Card.CardDefinition) => boolean): Card.CardDefinition[] {
-	const allCards = Card.getAvailableCards(StatsStore.getStats().unlockedUnits);
-	const filteredCards = filter ?
-		allCards.filter(filter) :
-		allCards;
-	const playerUnits = getState().gameData.player.units;
-	const maxRankCardIds = new Set(
-		playerUnits.filter((u) => u.rank >= 4).map((u) => u.cardId)
-	);
-
-	const availableCards = filteredCards.filter((card) => !maxRankCardIds.has(card.id));
-
-	return pickRandom(availableCards, count);
 }
