8ffac6d3afb161e0ea7cb9214076aa518cb3733c
Leonardo Farroco <mana-battle@users.noreply.github.com>
2026-03-20 13:07:49 -0300

    refactor(chara-events): centralize failed purchase animation duration

--- phaser/src/Systems/Chara/events.ts
+++ phaser/src/Systems/Chara/events.ts
@@ -4,6 +4,9 @@ import { tween } from "@Utils/animation";
 import { playSoundEffect } from "@Systems/AudioManager";
 import * as Shop from "@Systems/Shop";
 
+// Chara event animation durations
+const PURCHASE_FAILED_SNAP_DURATION_MS = 150;
+
 export const onDiscard = (unitId: string) => {
 	Shop.events.ownedUnitSold(unitId);
 };
@@ -13,7 +16,7 @@ export const onShopPurchaseFailed = (chara: Chara.Chara, vec: Vec2) => {
 	tween({
 		targets: [chara],
 		...vec,
-		duration: 150,
+		duration: PURCHASE_FAILED_SNAP_DURATION_MS,
 	});
 };
 
