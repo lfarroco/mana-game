6b6e71e0b3b215bafa5ff417313f5e34c1c2e7fd
Leonardo Farroco <mana-battle@users.noreply.github.com>
2026-05-21 12:34:39 -0300

    formatting

--- phaser/src/Engine/Scenes/Core/Core.ts
+++ phaser/src/Engine/Scenes/Core/Core.ts
@@ -11,65 +11,7 @@ export default class Core extends Phaser.Scene {
 
 	preload() {
 
-		const width = this.cameras.main.width;
-		const height = this.cameras.main.height;
-
-		const progressBar = this.add.graphics();
-		const progressBox = this.add.graphics();
-		progressBox.fillStyle(0x222222, 0.8);
-		progressBox.fillRect(width / 2 - 160, height / 2 - 25, 320, 50);
-
-		const loadingText = this.make.text({
-			x: width / 2,
-			y: height / 2 - 50,
-			text: "Loading...",
-			style: {
-				font: "20px monospace",
-				color: "#ffffff",
-			},
-		});
-		loadingText.setOrigin(0.5, 0.5);
-
-		const percentText = this.make.text({
-			x: width / 2,
-			y: height / 2,
-			text: "0%",
-			style: {
-				font: "18px monospace",
-				color: "#ffffff",
-			},
-		});
-		percentText.setOrigin(0.5, 0.5);
-
-		const assetText = this.make.text({
-			x: width / 2,
-			y: height / 2 + 50,
-			text: "",
-			style: {
-				font: "18px monospace",
-				color: "#ffffff",
-			},
-		});
-		assetText.setOrigin(0.5, 0.5);
-
-		this.load.on("progress", function (value: number) {
-			percentText.setText(parseInt(value * 100 + "") + "%");
-			progressBar.clear();
-			progressBar.fillStyle(0xffffff, 1);
-			progressBar.fillRect(width / 2 - 150, height / 2 - 15, 300 * value, 30);
-		});
-
-		this.load.on("fileprogress", function (file: { key: string }) {
-			assetText.setText("Loading asset: " + file.key);
-		});
-
-		this.load.on("complete", function () {
-			progressBar.destroy();
-			progressBox.destroy();
-			loadingText.destroy();
-			percentText.destroy();
-			assetText.destroy();
-		});
+        this.createLoadingBar();
 		
         if (DISABLE_ASSETS) return;
 
@@ -78,6 +20,54 @@ export default class Core extends Phaser.Scene {
         this.loadAudioAssets();
 
 	}
+
+    createLoadingBar(){
+
+          const width = this.cameras.main.width;
+          const height = this.cameras.main.height;
+
+          const progressBar = this.add.graphics();
+          const progressBox = this.add.graphics();
+          progressBox.fillStyle(0x222222, 0.8);
+          progressBox.fillRect(width / 2 - 160, height / 2 - 25, 320, 50);
+
+          const loadingText = this.make.text({
+              x: width / 2,
+              y: height / 2 - 50,
+              text: "Loading...",
+              style: {
+                  font: "20px monospace",
+                  color: "#ffffff",
+              },
+          });
+          loadingText.setOrigin(0.5, 0.5);
+
+          const percentText = this.make.text({
+              x: width / 2,
+              y: height / 2,
+              text: "0%",
+              style: {
+                  font: "18px monospace",
+                  color: "#ffffff",
+              },
+          });
+          percentText.setOrigin(0.5, 0.5);
+
+          this.load.on("progress", function (value: number) {
+              percentText.setText(parseInt(value * 100 + "") + "%");
+              progressBar.clear();
+              progressBar.fillStyle(0xffffff, 1);
+              progressBar.fillRect(width / 2 - 150, height / 2 - 15, 300 * value, 30);
+          });
+
+          this.load.on("complete", function () {
+              progressBar.destroy();
+              progressBox.destroy();
+              loadingText.destroy();
+              percentText.destroy();
+          });
+
+    }
 
     loadUIAssets() {
 
@@ -165,9 +155,7 @@ export default class Core extends Phaser.Scene {
     }
 
 	create() {
-		const collection = BASE_COLLECTION_DATA;
-
-		registerCollection(collection);
+		registerCollection(BASE_COLLECTION_DATA);
 
 		this.game.scene.start(SCENE_KEYS.TITLE);
 	}
